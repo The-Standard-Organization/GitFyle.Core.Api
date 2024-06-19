8eb2eddc979734929afd5d88db3c35c5a13aa58f
Christo du Toit <christo@dutoit.co.uk>
2024-06-19 22:05:08 +0100

    INFRA: Build Script

---
+++ GitFyle.Core.Api/GitFyle.Core.Infrastructure/Program.cs
@@ -0,0 +1,17 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using GitFyle.Core.Infrastructure.Services;
+
+namespace GitFyle.Core.Infrastructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var scriptGenerationService = new ScriptGenerationService();
+            scriptGenerationService.GenerateBuildScript();
+        }
+    }
+}
