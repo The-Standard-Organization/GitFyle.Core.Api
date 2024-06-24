13f066a1342936c6a886da917ee7c1c561e7127a
Lezune <jakeleque@gmail.com>
2024-06-24 16:55:02 -0700

    Added Contributors storage broker internl partial class.

---
+++ GitFyle.Core.Api/Brokers/Storages/StorageBroker.Contributors.cs
@@ -0,0 +1,10 @@
+﻿using GitFyle.Core.Api.Models.Foundations.Contributor;
+using Microsoft.EntityFrameworkCore;
+
+namespace GitFyle.Core.Api.Brokers.Storages
+{
+    internal partial class StorageBroker
+    {
+        public DbSet<Contributor> Contributors {  get; set; }
+    }
+}
--- GitFyle.Core.Api/Models/Foundations/Contributors/Contributor.cs
+++ GitFyle.Core.Api/Models/Foundations/Contributors/Contributor.cs
@@ -7,8 +7,7 @@ using System;
 namespace GitFyle.Core.Api.Models.Foundations.Contributor
 {
     public class Contributor 
-    {
-
+    { 
         public Guid Id { get; set; }
         public string ExternalId { get; set; }
         public Guid SourceId { get; set; }
@@ -16,6 +15,5 @@ namespace GitFyle.Core.Api.Models.Foundations.Contributor
         public string Name { get; set; }
         public string Email {  get; set; }
         public string AvatarUrl { get; set; }
-
     }
 }
