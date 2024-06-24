4d954caaccec4c1529d36f086454cdce07b9b068
Lezune <jakeleque@gmail.com>
2024-06-24 15:44:58 -0700

    Removed all unnecessary line breaks from class file

--- GitFyle.Core.Api/Models/Foundations/Contributor/Contributor.cs
+++ GitFyle.Core.Api/Models/Foundations/Contributor/Contributor.cs
@@ -10,17 +10,11 @@ namespace GitFyle.Core.Api.Models.Foundations.Contributor
     {
 
         public Guid Id { get; set; }
-        
         public string ExternalId { get; set; }
-
         public Guid SourceId { get; set; }
-
         public string Username { get; set; }
-
         public string Name { get; set; }
-
         public string Email {  get; set; }
-
         public string AvatarUrl { get; set; }
 
     }
