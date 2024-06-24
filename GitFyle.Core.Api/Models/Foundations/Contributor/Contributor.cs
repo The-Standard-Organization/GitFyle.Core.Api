fb130df0b2d8f43123b868f4fe1e7f25c0d23ea7
Lezune <jakeleque@gmail.com>
2024-06-24 15:40:29 -0700

    Created Contributor Model

---
+++ GitFyle.Core.Api/Models/Foundations/Contributor/Contributor.cs
@@ -0,0 +1,27 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System;
+
+namespace GitFyle.Core.Api.Models.Foundations.Contributor
+{
+    public class Contributor 
+    {
+
+        public Guid Id { get; set; }
+        
+        public string ExternalId { get; set; }
+
+        public Guid SourceId { get; set; }
+
+        public string Username { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email {  get; set; }
+
+        public string AvatarUrl { get; set; }
+
+    }
+}
