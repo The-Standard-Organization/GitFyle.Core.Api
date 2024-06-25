85aa9a2d49a85845ff02b2758ec4326f50c8bf81
Lezune <jakeleque@gmail.com>
2024-06-25 13:26:41 -0700

    Added Copyright comment

--- GitFyle.Core.Api/Brokers/Loggings/ILoggingBroker.cs
+++ GitFyle.Core.Api/Brokers/Loggings/ILoggingBroker.cs
@@ -1,5 +1,7 @@
-﻿using System;
-
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+using System;
 
 namespace GitFyle.Core.Api.Brokers.Loggings
 {
--- GitFyle.Core.Api/Brokers/Loggings/LoggingBroker.cs
+++ GitFyle.Core.Api/Brokers/Loggings/LoggingBroker.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.Extensions.Logging;
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using Microsoft.Extensions.Logging;
 
 namespace GitFyle.Core.Api.Brokers.Loggings
 {
