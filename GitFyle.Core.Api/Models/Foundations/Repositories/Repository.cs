0d91394006b1338714ccf64e65004c46a559265d
Christo du Toit <christo@dutoit.co.uk>
2024-06-27 00:58:06 +0100

    CODE RUB: Added CreatedAt and UpdatedAt

--- GitFyle.Core.Api/Models/Foundations/Repositories/Repository.cs
+++ GitFyle.Core.Api/Models/Foundations/Repositories/Repository.cs
@@ -19,6 +19,8 @@ public class Repository
     public string Token { get; set; }
     public DateTimeOffset TokenExpireAt { get; set; }
     public string Description { get; set; }
+    public DateTimeOffset CreatedAt { get; set; }
+    public DateTimeOffset UpdatedAt { get; set; }
 
     public Source Source { get; set; }
 }
