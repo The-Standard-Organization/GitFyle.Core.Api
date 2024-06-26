edf1af38680164262dd19dddf948ae1a22a485fc
Lezune <jakeleque@gmail.com>
2024-06-26 11:28:14 -0700

    Code Rub: added line break after copyright comment.

--- GitFyle.Core.Api/Brokers/Loggings/ILoggingBroker.cs
+++ GitFyle.Core.Api/Brokers/Loggings/ILoggingBroker.cs
@@ -1,6 +1,7 @@
 ﻿// ----------------------------------------------------------------------------------
 // Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
 // ----------------------------------------------------------------------------------
+
 using System;
 
 namespace GitFyle.Core.Api.Brokers.Loggings
