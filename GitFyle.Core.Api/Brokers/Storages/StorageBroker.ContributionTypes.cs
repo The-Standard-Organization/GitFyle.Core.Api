c0d5905b920d65d1e491fc9b5f1d76a5eac59f0b
kandarp.gautam@gmail.com <kandarp.gautam@gmail.com>
2024-06-26 20:28:59 +0530

    added copyright

--- GitFyle.Core.Api/Brokers/Storages/StorageBroker.ContributionTypes.cs
+++ GitFyle.Core.Api/Brokers/Storages/StorageBroker.ContributionTypes.cs
@@ -1,4 +1,8 @@
-﻿using GitFyle.Core.Api.Models.Foundations.ContributionTypes;
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using GitFyle.Core.Api.Models.Foundations.ContributionTypes;
 using Microsoft.EntityFrameworkCore;
 
 namespace GitFyle.Core.Api.Brokers.Storages
