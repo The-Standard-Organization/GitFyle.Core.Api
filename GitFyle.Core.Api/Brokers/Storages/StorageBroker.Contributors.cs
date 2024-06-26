c73e411b6d193d087f0bf1128f3fd1fff987f865
Lezune <jakeleque@gmail.com>
2024-06-26 11:31:06 -0700

    Added copyright comment

--- GitFyle.Core.Api/Brokers/Storages/StorageBroker.Contributors.cs
+++ GitFyle.Core.Api/Brokers/Storages/StorageBroker.Contributors.cs
@@ -1,4 +1,8 @@
-﻿using GitFyle.Core.Api.Models.Foundations.Contributor;
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using GitFyle.Core.Api.Models.Foundations.Contributor;
 using Microsoft.EntityFrameworkCore;
 
 namespace GitFyle.Core.Api.Brokers.Storages
