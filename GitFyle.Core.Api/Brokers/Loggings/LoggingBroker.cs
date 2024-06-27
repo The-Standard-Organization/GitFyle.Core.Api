3c75a6b34428407fa68e70a002b85210e57a736a
slimahmad6 <slimahmad6@outlook.com>
2024-06-27 08:36:00 +0100

    CODERUB: Seperate two liners

--- GitFyle.Core.Api/Brokers/Loggings/LoggingBroker.cs
+++ GitFyle.Core.Api/Brokers/Loggings/LoggingBroker.cs
@@ -16,10 +16,13 @@ namespace GitFyle.Core.Api.Brokers.Loggings
 
         public void LogTrace(string message) =>
             this.logger.LogTrace(message);
+
         public void LogDebug(string message) =>
             this.logger.LogDebug(message);
+
         public void LogWarning(string message) =>
             this.logger.LogWarning(message);
+
         public void LogCritical(Exception exception) =>
             this.logger.LogCritical(exception, exception.Message);
     }
