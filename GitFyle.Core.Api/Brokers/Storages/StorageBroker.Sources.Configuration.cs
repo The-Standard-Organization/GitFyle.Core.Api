0c78ca5d09e2e42965b58649224436dd30a25b60
Kandarp Gautam <kandarp.gautam@gmail.com>
2024-06-28 02:15:20 +0530

    CODE RUB: Remove Extra line.

--- GitFyle.Core.Api/Brokers/Storages/StorageBroker.Sources.Configuration.cs
+++ GitFyle.Core.Api/Brokers/Storages/StorageBroker.Sources.Configuration.cs
@@ -2,7 +2,6 @@
 // Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
 // ----------------------------------------------------------------------------------
 
-
 using GitFyle.Core.Api.Models.Foundations.Sources;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
