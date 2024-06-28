3fad583c6ec811d87f7f2d5296f6058e125c627d
slimahmad6 <slimahmad6@outlook.com>
2024-06-28 16:46:28 +0100

    BROKERS: LoggingBroker - LogTrace

---
+++ GitFyle.Core.Api/Brokers/Loggings/ILoggingBroker.cs
@@ -0,0 +1,11 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+namespace GitFyle.Core.Api.Brokers.Loggings
+{
+    public interface ILoggingBroker
+    {
+        void LogTrace(string message);
+    }
+}
---
+++ GitFyle.Core.Api/Brokers/Loggings/LoggingBroker.cs
@@ -0,0 +1,19 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using Microsoft.Extensions.Logging;
+
+namespace GitFyle.Core.Api.Brokers.Loggings
+{
+    public class LoggingBroker : ILoggingBroker
+    {
+        private readonly ILogger<LoggingBroker> logger;
+
+        public LoggingBroker(ILogger<LoggingBroker> logger) =>
+            this.logger = logger;
+
+        public void LogTrace(string message) =>
+            this.logger.LogTrace(message);
+    }
+}
