e65cca383ca80591c3f6c51ae7f6c25dbda3d2aa
slimahmad6 <slimahmad6@outlook.com>
2024-06-29 17:58:46 +0100

    CODERUB: Removed logCritical from interface

--- GitFyle.Core.Api/Brokers/Loggings/ILoggingBroker.cs
+++ GitFyle.Core.Api/Brokers/Loggings/ILoggingBroker.cs
@@ -12,6 +12,5 @@ namespace GitFyle.Core.Api.Brokers.Loggings
         void LogTrace(string message);
         void LogDebug(string message);
         void LogWarning(string message);
-        void LogCritical(Exception exception);
     }
 }
