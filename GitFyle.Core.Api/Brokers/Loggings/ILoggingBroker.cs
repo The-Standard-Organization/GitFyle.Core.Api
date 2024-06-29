3e4c8885d097076d7bc4f7f0b1bef5f5ad62fcc4
slimahmad6 <slimahmad6@outlook.com>
2024-06-29 10:56:42 +0100

    BROKERS: LoggingBroker-LogCritical

--- GitFyle.Core.Api/Brokers/Loggings/ILoggingBroker.cs
+++ GitFyle.Core.Api/Brokers/Loggings/ILoggingBroker.cs
@@ -12,6 +12,7 @@ namespace GitFyle.Core.Api.Brokers.Loggings
         void LogTrace(string message);
         void LogDebug(string message);
         void LogWarning(string message);
+        void LogCritical(Exception exception);
     }
 }
 
