0d328eafb9851af7107426efbe109cf2200b0b05
Hassan Habib <hassanhabib@live.com>
2024-07-01 13:40:00 -0700

    BROKERS: Insert Source
    Closes #47

---
+++ GitFyle.Core.Api/Brokers/Storages/IStorageBroker.Sources.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using GitFyle.Core.Api.Models.Foundations.Sources;
+
+namespace GitFyle.Core.Api.Brokers.Storages
+{
+    internal partial interface IStorageBroker
+    {
+        ValueTask<Source> InsertSourceAsync(Source source);
+    }
+}
--- GitFyle.Core.Api/Brokers/Storages/StorageBroker.Sources.cs
+++ GitFyle.Core.Api/Brokers/Storages/StorageBroker.Sources.cs
@@ -2,6 +2,7 @@
 // Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
 // ----------------------------------------------------------------------------------
 
+using System.Threading.Tasks;
 using GitFyle.Core.Api.Models.Foundations.Sources;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,5 +11,8 @@ namespace GitFyle.Core.Api.Brokers.Storages
     internal partial class StorageBroker
     {
         public DbSet<Source> Sources { get; set; }
+
+        public ValueTask<Source> InsertSourceAsync(Source source) =>
+            InsertAsync(source);
     }
 }
