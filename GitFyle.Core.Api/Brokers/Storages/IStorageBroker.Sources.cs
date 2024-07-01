30faf1482dc4f0df35eb8f0caea02ff3f86adc8b
Hassan Habib <hassanhabib@live.com>
2024-07-01 13:41:56 -0700

    CODE RUB: Add Copyrights

--- GitFyle.Core.Api/Brokers/Storages/IStorageBroker.Sources.cs
+++ GitFyle.Core.Api/Brokers/Storages/IStorageBroker.Sources.cs
@@ -1,4 +1,8 @@
-﻿using System.Threading.Tasks;
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
 using GitFyle.Core.Api.Models.Foundations.Sources;
 
 namespace GitFyle.Core.Api.Brokers.Storages
