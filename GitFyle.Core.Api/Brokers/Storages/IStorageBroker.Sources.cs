f7fe4b892444cc3e083d7aef409b6da5fe6fc833
slimahmad6 <slimahmad6@outlook.com>
2024-07-02 13:00:52 +0100

    CODERUB: Added SelectSourceByIdAsync method to interface

--- GitFyle.Core.Api/Brokers/Storages/IStorageBroker.Sources.cs
+++ GitFyle.Core.Api/Brokers/Storages/IStorageBroker.Sources.cs
@@ -2,6 +2,7 @@
 // Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
 // ----------------------------------------------------------------------------------
 
+using System;
 using System.Threading.Tasks;
 using GitFyle.Core.Api.Models.Foundations.Sources;
 
@@ -10,5 +11,8 @@ namespace GitFyle.Core.Api.Brokers.Storages
     internal partial interface IStorageBroker
     {
         ValueTask<Source> InsertSourceAsync(Source source);
+
+        ValueTask<Source> SelectSourceByIdAsync(
+            Guid sourceId);
     }
 }
