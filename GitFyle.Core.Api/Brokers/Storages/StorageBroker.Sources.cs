87e738e0fe0de07249e2d40b42a487519a9ca0fb
slimahmad6 <slimahmad6@outlook.com>
2024-07-02 13:41:42 +0100

    CODERUB: Updated selectSourceById

--- GitFyle.Core.Api/Brokers/Storages/StorageBroker.Sources.cs
+++ GitFyle.Core.Api/Brokers/Storages/StorageBroker.Sources.cs
@@ -16,12 +16,7 @@ namespace GitFyle.Core.Api.Brokers.Storages
         public ValueTask<Source> InsertSourceAsync(Source source) =>
             InsertAsync(source);
 
-        public async ValueTask<Source> SelectSourceByIdAsync(Guid sourceId)
-        {
-            using var broker = new StorageBroker(this.configuration);
-            broker.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
-
-            return await Sources.FindAsync(sourceId);
-        }
+        public async ValueTask<Source> SelectSourceByIdAsync(Guid sourceId) =>
+          await SelectAsync<Source>(sourceId);
     }
 }
