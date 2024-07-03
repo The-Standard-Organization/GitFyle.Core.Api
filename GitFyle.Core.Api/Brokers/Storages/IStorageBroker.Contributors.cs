2519fd1d0d59980fd1f564a1340649bad6f9f9d9
Lezune <jakeleque@gmail.com>
2024-07-02 19:49:28 -0700

    Added default interface method

--- GitFyle.Core.Api/Brokers/Storages/IStorageBroker.Contributors.cs
+++ GitFyle.Core.Api/Brokers/Storages/IStorageBroker.Contributors.cs
@@ -1,7 +1,11 @@
-﻿namespace GitFyle.Core.Api.Brokers.Storages
+﻿using GitFyle.Core.Api.Models.Foundations.Contributors;
+using GitFyle.Core.Api.Models.Foundations.Sources;
+using System.Threading.Tasks;
+
+namespace GitFyle.Core.Api.Brokers.Storages
 {
     internal partial interface IStorageBroker   
     {
-
+        ValueTask<Contributor> InsertContributorAsync(Contributor contributor);
     }
 }
