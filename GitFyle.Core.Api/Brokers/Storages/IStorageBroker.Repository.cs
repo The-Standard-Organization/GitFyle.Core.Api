5a29939e0f8d3b8dacfb4baa30c93a7cf134a1a2
Lezune <jakeleque@gmail.com>
2024-07-03 04:24:13 -0700

    Broker: SelectAllRepositoriesAsync - Method Added to StorageBroker.

--- GitFyle.Core.Api/Brokers/Storages/IStorageBroker.Repository.cs
+++ GitFyle.Core.Api/Brokers/Storages/IStorageBroker.Repository.cs
@@ -1,7 +1,10 @@
-﻿namespace GitFyle.Core.Api.Brokers.Storages
+﻿using GitFyle.Core.Api.Models.Foundations.Repositories;
+using System.Linq;
+
+namespace GitFyle.Core.Api.Brokers.Storages
 {
     internal partial interface IStorageBroker
     {
-
+        IQueryable<Repository> SelectAllRepositoriesAsync();
     }
 }
--- GitFyle.Core.Api/Brokers/Storages/StorageBroker.Repositories.cs
+++ GitFyle.Core.Api/Brokers/Storages/StorageBroker.Repositories.cs
@@ -4,12 +4,16 @@
 
 using GitFyle.Core.Api.Models.Foundations.Repositories;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace GitFyle.Core.Api.Brokers.Storages
 {
     internal partial class StorageBroker
     {
         public DbSet<Repository> Repositories { get; set; }
+
+        public IQueryable<Repository> SelectAllRepositoriesAsync() =>
+            SelectAll<Repository>();
     }
 }
 
