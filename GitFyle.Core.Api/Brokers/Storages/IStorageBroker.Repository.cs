6c2d77ff2ec5e122af1bc07bf8ef4218c283c87e
Lezune <jakeleque@gmail.com>
2024-07-03 04:26:03 -0700

    Code Rub: Added Copyright header

--- GitFyle.Core.Api/Brokers/Storages/IStorageBroker.Repository.cs
+++ GitFyle.Core.Api/Brokers/Storages/IStorageBroker.Repository.cs
@@ -1,4 +1,8 @@
-﻿using GitFyle.Core.Api.Models.Foundations.Repositories;
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using GitFyle.Core.Api.Models.Foundations.Repositories;
 using System.Linq;
 
 namespace GitFyle.Core.Api.Brokers.Storages
