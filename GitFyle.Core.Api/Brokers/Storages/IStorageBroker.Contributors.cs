ded32023e05f4e2d08d5d3091f1381c933cdb878
Lezune <jakeleque@gmail.com>
2024-07-02 20:28:59 -0700

    Added SelectContributorByIdAsync method to StorageBroker

--- GitFyle.Core.Api/Brokers/Storages/IStorageBroker.Contributors.cs
+++ GitFyle.Core.Api/Brokers/Storages/IStorageBroker.Contributors.cs
@@ -1,7 +1,11 @@
-﻿namespace GitFyle.Core.Api.Brokers.Storages
+﻿using GitFyle.Core.Api.Models.Foundations.Contributors;
+using System;
+using System.Threading.Tasks;
+
+namespace GitFyle.Core.Api.Brokers.Storages
 {
     internal partial interface IStorageBroker   
     {
-
+        public ValueTask<Contributor> SelectContributorByIdAsync(Guid contributorId);
     }
 }
