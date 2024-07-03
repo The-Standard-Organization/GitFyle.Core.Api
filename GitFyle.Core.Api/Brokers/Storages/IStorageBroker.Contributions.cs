acc0ae92339023baea7f584a9921f4d3e03dcaad
Kandarp Gautam <kandarp.gautam@gmail.com>
2024-07-03 17:21:31 +0530

    BROKERS: Select All Contributions

--- GitFyle.Core.Api/Brokers/Storages/IStorageBroker.Contributions.cs
+++ GitFyle.Core.Api/Brokers/Storages/IStorageBroker.Contributions.cs
@@ -3,17 +3,12 @@
 // ----------------------------------------------------------------------------------
 
 using System.Linq;
-using System;
 using GitFyle.Core.Api.Models.Foundations.Contributions;
-using Microsoft.EntityFrameworkCore;
 
 namespace GitFyle.Core.Api.Brokers.Storages
 {
-    internal partial class StorageBroker
+    internal partial interface IStorageBroker
     {
-        public DbSet<Contribution> Contributions { get; set; }
-
-        public IQueryable<Contribution> SelectAllContributionsAsync() =>
-            SelectAll<Contribution>();
+        IQueryable<Contribution> SelectAllContributionsAsync();
     }
 }
