ab161044f65efd299d6a3b51d43692f2c82333e7
Lezune <jakeleque@gmail.com>
2024-07-03 04:30:27 -0700

    Broker: SelectRepositoryByIdAsync - Method Added to StorageBroker.

--- GitFyle.Core.Api/Brokers/Storages/IStorageBroker.Repository.cs
+++ GitFyle.Core.Api/Brokers/Storages/IStorageBroker.Repository.cs
@@ -2,10 +2,14 @@
 // Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
 // ----------------------------------------------------------------------------------
 
+using GitFyle.Core.Api.Models.Foundations.Repositories;
+using System;
+using System.Threading.Tasks;
+
 namespace GitFyle.Core.Api.Brokers.Storages
 {
     internal partial interface IStorageBroker
     {
-
+        ValueTask<Repository> SelectRepositoryByIdAsync(Guid repositoryId);
     }
 }
--- GitFyle.Core.Api/Brokers/Storages/StorageBroker.Repositories.cs
+++ GitFyle.Core.Api/Brokers/Storages/StorageBroker.Repositories.cs
@@ -4,12 +4,17 @@
 
 using GitFyle.Core.Api.Models.Foundations.Repositories;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
 
 namespace GitFyle.Core.Api.Brokers.Storages
 {
     internal partial class StorageBroker
     {
         public DbSet<Repository> Repositories { get; set; }
+
+        public async ValueTask<Repository> SelectRepositoryByIdAsync(Guid repositoryId) =>
+            await SelectAsync<Repository>(repositoryId);    
     }
 }
 
