145807d1f9db95ac718c317fe5673d6995d86cfb
Kandarp Gautam <kandarp.gautam@gmail.com>
2024-07-03 16:00:38 +0530

    BROKERS: Insert Contribution

--- GitFyle.Core.Api/Brokers/Storages/IStorageBroker.Contributions.cs
+++ GitFyle.Core.Api/Brokers/Storages/IStorageBroker.Contributions.cs
@@ -4,14 +4,11 @@
 
 using System.Threading.Tasks;
 using GitFyle.Core.Api.Models.Foundations.Contributions;
-using Microsoft.EntityFrameworkCore;
 
 namespace GitFyle.Core.Api.Brokers.Storages
 {
-    internal partial class StorageBroker
+    internal partial interface IStorageBroker
     {
-        public DbSet<Contribution> Contributions { get; set; }
-        public async ValueTask<Contribution> InsertContributionAsync(Contribution contribution) =>
-            await InsertAsync(contribution);
+        ValueTask<Contribution> InsertContributionAsync(Contribution contribution);
     }
 }
