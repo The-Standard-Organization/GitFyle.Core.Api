31dfa546e7bcdef40e759d63dd6a4e56294631a7
Kandarp Gautam <kandarp.gautam@gmail.com>
2024-07-03 17:07:15 +0530

    BROKERS: Delete Contribution

--- GitFyle.Core.Api/Brokers/Storages/IStorageBroker.Contributions.cs
+++ GitFyle.Core.Api/Brokers/Storages/IStorageBroker.Contributions.cs
@@ -4,15 +4,11 @@
 
 using System.Threading.Tasks;
 using GitFyle.Core.Api.Models.Foundations.Contributions;
-using Microsoft.EntityFrameworkCore;
 
 namespace GitFyle.Core.Api.Brokers.Storages
 {
-    internal partial class StorageBroker
+    internal partial interface IStorageBroker
     {
-        public DbSet<Contribution> Contributions { get; set; }
-
-        public async ValueTask<Contribution> DeleteContributionAsync(Contribution contribution) =>
-            await DeleteAsync(contribution);
+        ValueTask<Contribution> DeleteContributionAsync(Contribution contribution);
     }
 }
