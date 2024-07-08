89571ba1dc81a8e286cdc0c10b114fa9937c85cd
Lezune <jakeleque@gmail.com>
2024-07-08 13:39:49 -0700

    Code Rub: Moved SelectByIdAsync Method from IStorageBroker.Repository to Repositories and deleted the Repository file.

--- GitFyle.Core.Api/Brokers/Storages/IStorageBroker.Repositories.cs
+++ GitFyle.Core.Api/Brokers/Storages/IStorageBroker.Repositories.cs
@@ -2,6 +2,7 @@
 // Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
 // ----------------------------------------------------------------------------------
 
+using System;
 using System.Threading.Tasks;
 using GitFyle.Core.Api.Models.Foundations.Repositories;
 
@@ -10,5 +11,7 @@ namespace GitFyle.Core.Api.Brokers.Storages
     internal partial interface IStorageBroker
     {
         ValueTask<Repository> InsertRepositoryAsync(Repository repository);
+
+        ValueTask<Repository> SelectRepositoryByIdAsync(Guid repositoryId);
     }
 }
