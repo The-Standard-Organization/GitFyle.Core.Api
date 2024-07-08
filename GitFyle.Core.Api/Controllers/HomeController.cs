83b2851727e68e2aabd9b948eef177d156028464
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-07-08 20:26:47 +0500

    MINOR FIX: Corrected namespace with .net8 version. removed unsued namespace

--- GitFyle.Core.Api/Controllers/HomeController.cs
+++ GitFyle.Core.Api/Controllers/HomeController.cs
@@ -2,17 +2,15 @@
 // Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
 // ----------------------------------------------------------------------------------
 
-using GitFyle.Core.Api.Brokers.Storages;
 using Microsoft.AspNetCore.Mvc;
 
-namespace GitFyle.Core.Api.Controllers
+namespace GitFyle.Core.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class HomeController : ControllerBase
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class HomeController : ControllerBase
-    {
-        [HttpGet]
-        public ActionResult<string> Get() =>
-            Ok("Hello, Mario. The princess is in another castle.");
-    }
+    [HttpGet]
+    public ActionResult<string> Get() =>
+        Ok("Hello, Mario. The princess is in another castle.");
 }
