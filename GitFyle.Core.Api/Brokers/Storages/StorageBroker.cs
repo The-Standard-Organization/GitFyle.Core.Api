3077b61b885dd926e9e48754b90a1fdf83cdcb59
Lezune <jakeleque@gmail.com>
2024-07-29 01:07:59 -0700

    CodeRub: Removed the unused SelectAll method from StorageBroker. Method has been replaced by the SelectAllAsync method.

--- GitFyle.Core.Api/Brokers/Storages/StorageBroker.cs
+++ GitFyle.Core.Api/Brokers/Storages/StorageBroker.cs
@@ -56,8 +56,6 @@ namespace GitFyle.Core.Api.Brokers.Storages
             return @object;
         }
 
-        private IQueryable<T> SelectAll<T>() where T : class => this.Set<T>();
-
         private async ValueTask<IQueryable<T>> SelectAllAsync<T>() where T : class => 
             await Task.FromResult(this.Set<T>());
 
