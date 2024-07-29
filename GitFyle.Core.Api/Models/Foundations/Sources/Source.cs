dd691751aea995ec1847edb0ae753236f6cef6b4
Christo du Toit <christo@dutoit.co.uk>
2024-07-29 12:37:36 +0100

    DATA: Extended Source With Audit Properties

--- GitFyle.Core.Api/Models/Foundations/Sources/Source.cs
+++ GitFyle.Core.Api/Models/Foundations/Sources/Source.cs
@@ -14,6 +14,10 @@ namespace GitFyle.Core.Api.Models.Foundations.Sources
         public Guid Id { get; set; }
         public string Name { get; set; }
         public string Url { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTimeOffset CreatedDate { get; set; }
+        public string UpdatedBy { get; set; }
+        public DateTimeOffset UpdatedDate { get; set; }
         public IEnumerable<Contributor> Contributors { get; set; }
         public IEnumerable<Repository> Repositories { get; set; }
     }
