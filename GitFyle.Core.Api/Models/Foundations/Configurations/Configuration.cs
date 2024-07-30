afb1e01e4be016e94cbc59f09790423c84f57012
Umidjon <makhammadsoliyev@gmail.com>
2024-07-31 00:22:08 +0500

    DATA: Configuration

---
+++ GitFyle.Core.Api/Models/Foundations/Configurations/Configuration.cs
@@ -0,0 +1,20 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System;
+
+namespace GitFyle.Core.Api.Models.Foundations.Configurations
+{
+    public class Configuration
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Value { get; set; }
+        public string Type { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string UpdatedBy { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
+}
