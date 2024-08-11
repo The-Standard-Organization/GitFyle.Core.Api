16e5805155f8c5fa93dec90eb60c3d4c50e77561
Hassan Habib <hassanhabib@live.com>
2024-08-11 10:35:06 -0700

    ShouldThrowDependencyExceptionOnAddIfDependencyErrorOccurredAndLogItAsync -> FAIL

---
+++ GitFyle.Core.Api.Tests.Unit/Services/Foundations/Sources/FailedUpdateSourceException.cs
@@ -0,0 +1,21 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using Microsoft.EntityFrameworkCore;
+using Xeptions;
+
+namespace GitFyle.Core.Api.Tests.Unit.Services.Foundations.Sources
+{
+    internal class FailedOperationSourceException : Xeption
+    {
+        private string message;
+        private DbUpdateException innerException;
+
+        public FailedOperationSourceException(string message, DbUpdateException innerException)
+        {
+            this.message = message;
+            this.innerException = innerException;
+        }
+    }
+}
--- GitFyle.Core.Api.Tests.Unit/Services/Foundations/Sources/SourceServiceTests.Exceptions.Add.cs
+++ GitFyle.Core.Api.Tests.Unit/Services/Foundations/Sources/SourceServiceTests.Exceptions.Add.cs
@@ -8,6 +8,7 @@ using FluentAssertions;
 using GitFyle.Core.Api.Models.Foundations.Sources;
 using GitFyle.Core.Api.Models.Foundations.Sources.Exceptions;
 using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 
 namespace GitFyle.Core.Api.Tests.Unit.Services.Foundations.Sources
@@ -21,7 +22,7 @@ namespace GitFyle.Core.Api.Tests.Unit.Services.Foundations.Sources
             Source someSource = CreateRandomSource();
             SqlException sqlException = CreateSqlException();
 
-            var failedStoragSourceeException =
+            var failedStorageSourceException =
                 new FailedStorageSourceException(
                     message: "Failed source storage error occurred, contact support.",
                     innerException: sqlException);
@@ -29,7 +30,7 @@ namespace GitFyle.Core.Api.Tests.Unit.Services.Foundations.Sources
             var expectedSourceDependencyException =
                 new SourceDependencyException(
                     message: "Source dependency error occurred, contact support.",
-                    innerException: failedStoragSourceeException);
+                    innerException: failedStorageSourceException);
 
             this.dateTimeBrokerMock.Setup(broker =>
                 broker.GetCurrentDateTimeOffsetAsync())
@@ -111,6 +112,58 @@ namespace GitFyle.Core.Api.Tests.Unit.Services.Foundations.Sources
             this.loggingBrokerMock.Verify(broker =>
                 broker.LogErrorAsync(It.Is(SameExceptionAs(
                     expectedSourceDependencyValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertSourceAsync(It.IsAny<Source>()),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowDependencyExceptionOnAddIfDependencyErrorOccurredAndLogItAsync()
+        {
+            // given
+            Source someSource = CreateRandomSource();
+            var dbUpdateException = new DbUpdateException();
+
+            var failedOperationSourceException =
+                new FailedOperationSourceException(
+                    message: "Failed operation source  error occurred, contact support.",
+                    innerException: dbUpdateException);
+
+            var expectedSourceDependencyException =
+                new SourceDependencyException(
+                    message: "Source dependency error occurred, contact support.",
+                    innerException: failedOperationSourceException);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTimeOffsetAsync())
+                    .ThrowsAsync(dbUpdateException);
+
+            // when
+            ValueTask<Source> addSourceTask =
+                this.sourceService.AddSourceAsync(
+                    someSource);
+
+            SourceDependencyException actualSourceDependencyException =
+                await Assert.ThrowsAsync<SourceDependencyException>(
+                    testCode: addSourceTask.AsTask);
+
+            // then
+            actualSourceDependencyException.Should().BeEquivalentTo(
+                expectedSourceDependencyException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffsetAsync(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedSourceDependencyException))),
                         Times.Once);
 
             this.storageBrokerMock.Verify(broker =>
