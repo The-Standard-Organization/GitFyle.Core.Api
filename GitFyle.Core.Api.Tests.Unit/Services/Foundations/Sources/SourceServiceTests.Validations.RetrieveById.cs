aae8012e76d0e6a130908706d0d74ab9dc974b45
Greg Hays <glhays.dev@gmail.com>
2024-08-15 11:14:33 -0700

    ShouldThrowValidationExceptionOnRetrieveByIdWhenSourceIdIsInvalidAndLogItAsync => FAIL

---
+++ GitFyle.Core.Api.Tests.Unit/Services/Foundations/Sources/SourceServiceTests.Validations.RetrieveById.cs
@@ -0,0 +1,63 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using GitFyle.Core.Api.Models.Foundations.Sources;
+using GitFyle.Core.Api.Models.Foundations.Sources.Exceptions;
+using Moq;
+
+namespace GitFyle.Core.Api.Tests.Unit.Services.Foundations.Sources
+{
+    public partial class SourceServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnRetrieveByIdWhenSourceIdIsInvalidAndLogItAsync()
+        {
+            // given
+            var invalidSourceId = Guid.Empty;
+
+            var invalidSourceException = new InvalidSourceException(
+                message: "Invalid Source Id, correct Id to continue");
+
+            invalidSourceException.AddData(
+                key: nameof(Source.Id),
+                values: "Id is required");
+
+            var expectedSourceValidationException =
+                new SourceValidationException(
+                    message: "Source validation error occurred, fix errors and try again.",
+                    innerException: invalidSourceException);
+
+            // when
+            ValueTask<Source> retrieveSourceByIdTask =
+                this.sourceService.RetrieveSourceByIdAsync(invalidSourceId);
+
+            SourceValidationException actualSourceValidationException =
+                await Assert.ThrowsAsync<SourceValidationException>(
+                    retrieveSourceByIdTask.AsTask);
+
+            // then
+            await Assert.ThrowsAsync<SourceValidationException>(
+                retrieveSourceByIdTask.AsTask);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedSourceValidationException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectSourceByIdAsync(It.IsAny<Guid>()),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffsetAsync(),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
