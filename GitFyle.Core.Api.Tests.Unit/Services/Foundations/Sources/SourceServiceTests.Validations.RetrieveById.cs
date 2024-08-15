03a1efbbf2e48332ce42ab227e38908912e65018
Greg Hays <glhays.dev@gmail.com>
2024-08-15 12:54:34 -0700

    ShouldThrowValidationExceptionOnRetrieveByIdIfSourceIdNotFoundAndLogitAsync => FAIL

--- GitFyle.Core.Api.Tests.Unit/Services/Foundations/Sources/SourceServiceTests.Validations.RetrieveById.cs
+++ GitFyle.Core.Api.Tests.Unit/Services/Foundations/Sources/SourceServiceTests.Validations.RetrieveById.cs
@@ -8,6 +8,7 @@ using FluentAssertions;
 using GitFyle.Core.Api.Models.Foundations.Sources;
 using GitFyle.Core.Api.Models.Foundations.Sources.Exceptions;
 using Moq;
+using Xeptions;
 
 namespace GitFyle.Core.Api.Tests.Unit.Services.Foundations.Sources
 {
@@ -59,6 +60,54 @@ namespace GitFyle.Core.Api.Tests.Unit.Services.Foundations.Sources
 
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnRetrieveByIdIfSourceIdNotFoundAndLogitAsync()
+        {
+            //given
+            var someSourceId = Guid.NewGuid();
+            Source nullSource = null;
+            var innerException = new Exception();
+
+            var notFoundSourceException =
+                new NotFoundSourceException(
+                    message: $"Source not found with id: {someSourceId}",
+                    innerException: innerException.InnerException.As<Xeption>());
+
+            var expectedSourceValidationException =
+                new SourceValidationException(
+                    message: "Source validation error occurred, fix errors and try again.",
+                    innerException: notFoundSourceException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectSourceByIdAsync(someSourceId))
+                    .ReturnsAsync(nullSource);
+        
+            // when
+            ValueTask<Source> retrieveSourceByIdTask =
+                this.sourceService.RetrieveSourceByIdAsync(someSourceId);
+
+            SourceValidationException actualSourceValidationException =
+                await Assert.ThrowsAsync<SourceValidationException>(
+                    retrieveSourceByIdTask.AsTask);
+
+            // then
+            actualSourceValidationException.Should().BeEquivalentTo(
+                expectedSourceValidationException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectSourceByIdAsync(someSourceId),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedSourceValidationException))),
+                    Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
     }
---
+++ GitFyle.Core.Api/Models/Foundations/Sources/Exceptions/NotFoundSourceException.cs
@@ -0,0 +1,16 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System;
+using Xeptions;
+
+namespace GitFyle.Core.Api.Models.Foundations.Sources.Exceptions
+{
+    public class NotFoundSourceException : Xeption
+    {
+        public NotFoundSourceException(string message, Exception innerException)
+            : base(message, innerException)
+        { }
+    }
+}
