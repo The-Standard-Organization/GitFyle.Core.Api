26915d210504707a9d52d77be9b0bccc29d2e322
Greg Hays <glhays.dev@gmail.com>
2024-08-17 12:16:32 -0700

    ShouldThrowCriticalDependencyExceptionOnRetrieveByIdIfSQLErrorOccursAndLogItAsync => PASS

---
+++ GitFyle.Core.Api.Tests.Unit/Services/Foundations/Sources/SourceServiceTests.Exceptions.RetrieveById.cs
@@ -0,0 +1,62 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using GitFyle.Core.Api.Models.Foundations.Sources;
+using GitFyle.Core.Api.Models.Foundations.Sources.Exceptions;
+using Moq;
+
+namespace GitFyle.Core.Api.Tests.Unit.Services.Foundations.Sources
+{
+    public partial class SourceServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowCriticalDependencyExceptionOnRetrieveByIdIfSQLErrorOccursAndLogItAsync()
+        {
+            // given
+            Guid someGuid = Guid.NewGuid();
+            var sqlException = CreateSqlException();
+
+            var failedStorageSourceException =
+                new FailedStorageSourceException(
+                    message: "Failed source storage error occurred, contact support.",
+                    innerException: sqlException);
+
+            var expectedSourceDependencyException =
+                new SourceDependencyException(
+                    message: "Source dependency error occurred, contact support.",
+                    innerException: failedStorageSourceException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectSourceByIdAsync(someGuid))
+                        .ThrowsAsync(sqlException);
+
+            // when
+            ValueTask<Source> retrieveSourceByIdTask =
+                this.sourceService.RetrieveSourceByIdAsync(someGuid);
+
+            // then
+            await Assert.ThrowsAsync<SourceDependencyException>(() =>
+                retrieveSourceByIdTask.AsTask());
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectSourceByIdAsync(someGuid),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCriticalAsync(It.Is(SameExceptionAs(
+                    expectedSourceDependencyException))),
+                    Times.Once);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffsetAsync(),
+                    Times.Never);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
