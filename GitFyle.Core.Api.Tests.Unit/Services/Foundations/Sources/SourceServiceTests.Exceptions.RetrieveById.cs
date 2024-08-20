e55979860344a80c380200b4dcbab3a1078a6915
Greg Hays <glhays.dev@gmail.com>
2024-08-20 12:10:15 -0700

    ShouldThrowServiceExceptionOnRetrieveByIdIfServiceErrorOccursAndLogItAsync => PASS

--- GitFyle.Core.Api.Tests.Unit/Services/Foundations/Sources/SourceServiceTests.Exceptions.RetrieveById.cs
+++ GitFyle.Core.Api.Tests.Unit/Services/Foundations/Sources/SourceServiceTests.Exceptions.RetrieveById.cs
@@ -4,6 +4,7 @@
 
 using System;
 using System.Threading.Tasks;
+using FluentAssertions;
 using GitFyle.Core.Api.Models.Foundations.Sources;
 using GitFyle.Core.Api.Models.Foundations.Sources.Exceptions;
 using Moq;
@@ -48,6 +49,57 @@ namespace GitFyle.Core.Api.Tests.Unit.Services.Foundations.Sources
             this.loggingBrokerMock.Verify(broker =>
                 broker.LogCriticalAsync(It.Is(SameExceptionAs(
                     expectedSourceDependencyException))),
+                    Times.Once);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffsetAsync(),
+                    Times.Never);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowServiceExceptionOnRetrieveByIdIfServiceErrorOccursAndLogItAsync()
+        {
+            //given
+            Guid someGuid = Guid.NewGuid();
+            var serviceException = new Exception();
+
+            var failedServiceSourceException =
+                new FailedServiceSourceException(
+                    message: "Failed service source error occurred, contact support.",
+                    innerException: serviceException);
+
+            var expectedSourceServiceException =
+                new SourceServiceException(
+                    message: "Service error occurred, contact support.",
+                    innerException: failedServiceSourceException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectSourceByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(serviceException);
+
+            //when
+            ValueTask<Source> retrieveSourceByIdTask =
+                this.sourceService.RetrieveSourceByIdAsync(someGuid);
+
+            SourceServiceException actualSourceServiceException =
+                await Assert.ThrowsAsync<SourceServiceException>(
+                    retrieveSourceByIdTask.AsTask);
+
+            //then
+            actualSourceServiceException.Should().BeEquivalentTo(
+                expectedSourceServiceException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectSourceByIdAsync(It.IsAny<Guid>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+            broker.LogErrorAsync(It.Is(SameExceptionAs(
+                expectedSourceServiceException))),
                     Times.Once);
 
             this.dateTimeBrokerMock.Verify(broker =>
