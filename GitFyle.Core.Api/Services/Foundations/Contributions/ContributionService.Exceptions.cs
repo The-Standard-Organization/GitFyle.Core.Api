6927ec51d9b04658a30cbb94dc7b07b53f9a7268
slimahmad6 <slimahmad6@outlook.com>
2024-08-29 16:56:14 +0100

    ShouldThrowDependencyValidationExceptionOnAddIfContributionAlreadyExistsAndLogItAsync -> PASS

--- GitFyle.Core.Api/Services/Foundations/Contributions/ContributionService.Exceptions.cs
+++ GitFyle.Core.Api/Services/Foundations/Contributions/ContributionService.Exceptions.cs
@@ -1,5 +1,7 @@
 ﻿using System.Threading.Tasks;
+using EFxceptions.Models.Exceptions;
 using GitFyle.Core.Api.Models.Foundations.Contributions;
+using GitFyle.Core.Api.Models.Foundations.Contributions.Exceptions;
 using GitFyle.Core.Api.Models.Foundations.Contributions.Exceptions;
 using Microsoft.Data.SqlClient;
 using Xeptions;
@@ -32,6 +34,16 @@ namespace GitFyle.Core.Api.Services.Foundations.Contributions
 
                 throw await CreateAndLogCriticalDependencyExceptionAsync(failedStorageContributionException);
             }
+            catch (DuplicateKeyException duplicateKeyException)
+            {
+                var alreadyExistsContributionException =
+                    new AlreadyExistsContributionException(
+                        message: "Contribution already exists error occurred.",
+                        innerException: duplicateKeyException,
+                        data: duplicateKeyException.Data);
+
+                throw await CreateAndLogDependencyValidationExceptionAsync(alreadyExistsContributionException);
+            }
         }
 
 
@@ -57,6 +69,18 @@ namespace GitFyle.Core.Api.Services.Foundations.Contributions
             await this.loggingBroker.LogCriticalAsync(contributionDependencyException);
 
             return contributionDependencyException;
+        }
+
+        private async ValueTask<ContributionDependencyValidationException> CreateAndLogDependencyValidationExceptionAsync(
+        Xeption exception)
+        {
+            var contributionDependencyValidationException = new ContributionDependencyValidationException(
+                message: "Contribution dependency validation error occurred, fix errors and try again.",
+                innerException: exception);
+
+            await this.loggingBroker.LogErrorAsync(contributionDependencyValidationException);
+
+            return contributionDependencyValidationException;
         }
 
     }
