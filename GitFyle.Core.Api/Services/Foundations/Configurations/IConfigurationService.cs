54a9c8c72a90693187e12a6655359e394d95e820
Pratik Kaje <kajepratik@gmail.com>
2024-08-30 23:24:22 +0400

    ShouldAddConfigurationAsync -> FAIL

---
+++ GitFyle.Core.Api/Services/Foundations/Configurations/IConfigurationService.cs
@@ -0,0 +1,14 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using GitFyle.Core.Api.Models.Foundations.Configurations;
+
+namespace GitFyle.Core.Api.Services.Foundations.Configurations
+{
+    public interface IConfigurationService
+    {
+        ValueTask<Configuration> AddConfigurationAsync(Configuration configuration);
+    }
+}
