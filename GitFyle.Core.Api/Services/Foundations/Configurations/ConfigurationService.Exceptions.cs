1856b605c35c9564f542ac480e70185ab0a1679c
Pratik Kaje <kajepratik@gmail.com>
2024-09-05 20:55:27 +0400

    ShouldThrowDependencyValidationExceptionOnAddIfConfigurationAlreadyExistsAndLogItAsync => PASS

--- GitFyle.Core.Api/Services/Foundations/Configurations/ConfigurationService.Exceptions.cs
+++ GitFyle.Core.Api/Services/Foundations/Configurations/ConfigurationService.Exceptions.cs
@@ -2,6 +2,7 @@
 // Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
 // ----------------------------------------------------------------------------------
 
+using EFxceptions.Models.Exceptions;
 using GitFyle.Core.Api.Models.Foundations.Configurations;
 using GitFyle.Core.Api.Models.Foundations.Configurations.Exceptions;
 using Microsoft.Data.SqlClient;
@@ -38,6 +39,28 @@ namespace GitFyle.Core.Api.Services.Foundations.Configurations
 
                 throw await CreateAndLogCriticalDependencyExceptionAsync(failedStorageConfigurationException);
             }
+            catch (DuplicateKeyException duplicateKeyException)
+            {
+                var alreadyExistsConfigurationException =
+                    new AlreadyExistsConfigurationException(
+                        message: "Configuration already exists error occurred.",
+                        innerException: duplicateKeyException,
+                        data: duplicateKeyException.Data);
+
+                throw await CreateAndLogDependencyValidationExceptionAsync(alreadyExistsConfigurationException);
+            }
+        }
+
+        private async ValueTask<ConfigurationDependencyValidationException> CreateAndLogDependencyValidationExceptionAsync(Xeption exception)
+        {
+            var configurationDependencyValidationException =
+                new ConfigurationDependencyValidationException(
+                    message: "Configuration dependency validation error occurred, fix errors and try again.",
+                    innerException: exception);
+
+            await this.loggingBroker.LogErrorAsync(configurationDependencyValidationException);
+
+            return configurationDependencyValidationException;
         }
 
         private async ValueTask<ConfigurationDependencyException> CreateAndLogCriticalDependencyExceptionAsync(Xeption exception)
