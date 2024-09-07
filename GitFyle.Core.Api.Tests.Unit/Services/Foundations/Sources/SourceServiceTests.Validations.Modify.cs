297321a194caf61e895d75671f79d1b246eda3bb
Greg Hays <glhays.dev@gmail.com>
2024-09-07 13:05:04 -0700

    ShouldThrowValidationExceptionOnModifyIfSourceIsInvalidAndLogItAsync => PASS

--- GitFyle.Core.Api.Tests.Unit/Services/Foundations/Sources/SourceServiceTests.Validations.Modify.cs
+++ GitFyle.Core.Api.Tests.Unit/Services/Foundations/Sources/SourceServiceTests.Validations.Modify.cs
@@ -6,6 +6,7 @@ using FluentAssertions;
 using GitFyle.Core.Api.Models.Foundations.Sources;
 using GitFyle.Core.Api.Models.Foundations.Sources.Exceptions;
 using Moq;
+using System;
 using System.Threading.Tasks;
 
 namespace GitFyle.Core.Api.Tests.Unit.Services.Foundations.Sources
@@ -51,6 +52,91 @@ namespace GitFyle.Core.Api.Tests.Unit.Services.Foundations.Sources
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnModifyIfSourceIsInvalidAndLogItAsync(string invalidString)
+        {
+            //given
+            var invalidSource = new Source
+            {
+                Name = invalidString,
+                Url = invalidString,
+                UpdatedBy = invalidString,
+                UpdatedDate = default,
+            };
+
+            var invalidSourceException = new InvalidSourceException(
+                message: "Source is invalid, fix the errors and try again.");
+
+            invalidSourceException.AddData(
+                key: nameof(Source.Id),
+                values: "Id is invalid");
+
+            invalidSourceException.AddData(
+                key: nameof(Source.Name),
+                values: "Text is required");
+
+            invalidSourceException.AddData(
+                key: nameof(Source.Url),
+                values: "Url is invalid");
+
+            invalidSourceException.AddData(
+                key: nameof(Source.CreatedBy),
+                values: "Text is required");
+
+            invalidSourceException.AddData(
+                key: nameof(Source.UpdatedBy),
+                values: "Text is required");
+
+            invalidSourceException.AddData(
+                key: nameof(Source.CreatedDate),
+                values:
+                [
+                    "Date is invalid",
+                    $"Date is the same as {nameof(Source.CreatedDate)}"
+                ]);
+
+            invalidSourceException.AddData(
+                key: nameof(Source.UpdatedDate),
+                values: "Date is invalid");
+
+            var expectedSourceValidationException =
+                new SourceValidationException(
+                    message: "Source validation error occurred, fix errors and try again.",
+                    innerException: invalidSourceException);
+
+            // when
+            ValueTask<Source> modifySourceTask =
+                this.sourceService.ModifySourceAsync(invalidSource);
+
+            SourceValidationException actualSourceValidationException =
+                await Assert.ThrowsAsync<SourceValidationException>(
+                    modifySourceTask.AsTask);
+
+            // then
+            actualSourceValidationException.Should().BeEquivalentTo(
+                expectedSourceValidationException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffsetAsync(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(
+                    SameExceptionAs(expectedSourceValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertSourceAsync(It.IsAny<Source>()),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
         }
     }
 }
