9a7437d0b23b5c4fe9db90713bab2f0be8015d75
Pratik Kaje <kajepratik@gmail.com>
2024-09-09 22:21:42 +0400

    CODE RUB: Formatting changes

--- GitFyle.Core.Api/Models/Foundations/Configurations/Exceptions/AlreadyExistsConfigurationException.cs
+++ GitFyle.Core.Api/Models/Foundations/Configurations/Exceptions/AlreadyExistsConfigurationException.cs
@@ -10,13 +10,8 @@ namespace GitFyle.Core.Api.Models.Foundations.Configurations.Exceptions
 {
     public class AlreadyExistsConfigurationException : Xeption
     {
-        public AlreadyExistsConfigurationException(
-            string message,
-            Exception innerException,
-            IDictionary data) :
-            base(message,
-                innerException,
-                data)
+        public AlreadyExistsConfigurationException(string message, Exception innerException, IDictionary data)
+            : base(message, innerException, data)
         { }
     }
 }
