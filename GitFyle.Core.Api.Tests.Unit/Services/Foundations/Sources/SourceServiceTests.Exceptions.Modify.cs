e12523965105be5fe9689a592360ba96fef511c8
Greg Hays <glhays.dev@gmail.com>
2024-09-09 17:20:00 -0700

    ShouldThrowDependencyExceptionOnModifyIfDatabaseUpdateExceptionOccursAndLogItAsync => PASS

--- GitFyle.Core.Api.Tests.Unit/Services/Foundations/Sources/SourceServiceTests.Exceptions.Modify.cs
+++ GitFyle.Core.Api.Tests.Unit/Services/Foundations/Sources/SourceServiceTests.Exceptions.Modify.cs
@@ -6,7 +6,9 @@ using FluentAssertions;
 using GitFyle.Core.Api.Models.Foundations.Sources;
 using GitFyle.Core.Api.Models.Foundations.Sources.Exceptions;
 using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using Moq;
+using System;
 using System.Threading.Tasks;
 
 namespace GitFyle.Core.Api.Tests.Unit.Services.Foundations.Sources
@@ -65,6 +67,69 @@ namespace GitFyle.Core.Api.Tests.Unit.Services.Foundations.Sources
 
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        private async Task ShouldThrowDependencyExceptionOnModifyIfDatabaseUpdateExceptionOccursAndLogItAsync()
+        {
+            // given
+            int minutesInPast = GetRandomNegativeNumber();
+            DateTimeOffset randomDateTimeOffset = GetRandomDateTimeOffset();
+
+            Source randomSource =
+                CreateRandomSource(randomDateTimeOffset);
+
+            randomSource.CreatedDate =
+                randomDateTimeOffset.AddMinutes(minutesInPast);
+
+            var dbUpdateException = new DbUpdateException();
+
+            var failedOperationSourceException =
+                new FailedOperationSourceException(
+                    message: "Failed operation source  error occurred, contact support.",
+                    innerException: dbUpdateException);
+
+            var expectedSourceDependencyException =
+                new SourceDependencyException(
+                    message: "Source dependency error occurred, contact support.",
+                    innerException: failedOperationSourceException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectSourceByIdAsync(randomSource.Id))
+                    .ThrowsAsync(dbUpdateException);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTimeOffsetAsync())
+                    .ReturnsAsync(randomDateTimeOffset);
+
+            // when
+            ValueTask<Source> modifySourceTask =
+                this.sourceService.ModifySourceAsync(randomSource);
+
+            SourceDependencyException actualSourceDependencyException =
+                await Assert.ThrowsAsync<SourceDependencyException>(
+                    modifySourceTask.AsTask);
+
+            // then
+            actualSourceDependencyException.Should().BeEquivalentTo(
+                expectedSourceDependencyException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectSourceByIdAsync(randomSource.Id),
+                    Times.Once);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffsetAsync(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedSourceDependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
         }
     }
