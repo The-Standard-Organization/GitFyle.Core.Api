da5276e40cfb85226b19cf77fc185aa61bfc013e
Greg Hays <glhays.dev@gmail.com>
2024-09-09 17:07:08 -0700

    ShouldThrowCriticalDependencyExceptionOnModifyIfSqlErrorOccursAndLogItAsync => PASS

---
+++ GitFyle.Core.Api.Tests.Unit/Services/Foundations/Sources/SourceServiceTests.Exceptions.Modify.cs
@@ -0,0 +1,71 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using FluentAssertions;
+using GitFyle.Core.Api.Models.Foundations.Sources;
+using GitFyle.Core.Api.Models.Foundations.Sources.Exceptions;
+using Microsoft.Data.SqlClient;
+using Moq;
+using System.Threading.Tasks;
+
+namespace GitFyle.Core.Api.Tests.Unit.Services.Foundations.Sources
+{
+    public partial class SourceServiceTests
+    {
+        [Fact]
+        private async Task ShouldThrowCriticalDependencyExceptionOnModifyIfSqlErrorOccursAndLogItAsync()
+        {
+            // given
+            Source someSource = CreateRandomSource();
+            SqlException sqlException = CreateSqlException();
+
+            var failedSourceStorageException =
+                new FailedStorageSourceException(
+                    message: "Failed source storage error occurred, contact support.",
+                        innerException: sqlException);
+
+            var expectedSourceDependencyException =
+                new SourceDependencyException(
+                    message: "Source dependency error occurred, contact support.",
+                        innerException: failedSourceStorageException);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTimeOffsetAsync())
+                    .ThrowsAsync(sqlException);
+
+            // when
+            ValueTask<Source> modifySourceTask =
+                this.sourceService.ModifySourceAsync(someSource);
+
+            SourceDependencyException actualSourceDependencyException =
+                await Assert.ThrowsAsync<SourceDependencyException>(
+                    modifySourceTask.AsTask);
+
+            // then
+            actualSourceDependencyException.Should().BeEquivalentTo(
+                expectedSourceDependencyException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffsetAsync(),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectSourceByIdAsync(someSource.Id),
+                    Times.Never);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCriticalAsync(It.Is(SameExceptionAs(
+                    expectedSourceDependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateSourceAsync(someSource),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
