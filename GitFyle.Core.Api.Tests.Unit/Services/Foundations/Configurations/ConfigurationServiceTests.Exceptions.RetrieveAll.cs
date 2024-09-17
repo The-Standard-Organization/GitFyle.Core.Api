7b8f4f4651b03aeca1497db570dcd30300f354ca
Pratik Kaje <kajepratik@gmail.com>
2024-09-17 19:46:18 +0400

    ShouldThrowCriticalDependencyExceptionOnRetrieveAllIfSQLExceptionOccursAndLogItAsync => FAIL

---
+++ GitFyle.Core.Api.Tests.Unit/Services/Foundations/Configurations/ConfigurationServiceTests.Exceptions.RetrieveAll.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GitFyle.Core.Api.Models.Foundations.Configurations;
+using GitFyle.Core.Api.Models.Foundations.Configurations.Exceptions;
+using Microsoft.Data.SqlClient;
+using Moq;
+
+namespace GitFyle.Core.Api.Tests.Unit.Services.Foundations.Configurations
+{
+    public partial class ConfigurationServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowCriticalDependencyExceptionOnRetrieveAllIfSQLExceptionOccursAndLogItAsync()
+        {
+            // given
+            SqlException sqlException = CreateSqlException();
+
+            var failedStorageConfigurationException = 
+                new FailedStorageConfigurationException(
+                    message: "Failed configuration storage error occurred, contact support.",
+                    innerException: sqlException);
+
+            var expectedConfigurationDependencyException =
+                new ConfigurationDependencyException(
+                    message: "Configuration dependency error occurred, contact support.",
+                    innerException: failedStorageConfigurationException);
+
+            this.storageBrokerMock.Setup(broker => 
+                broker.SelectAllConfigurationsAsync())
+                    .ThrowsAsync(sqlException);
+
+            // when
+            ValueTask<IQueryable<Configuration>> retrieveAllConfigurationsTask = 
+                this.configurationService.RetrieveAllConfigurationsAsync();
+
+            // then
+            ConfigurationDependencyException actualConfigurationDependencyException =
+                await Assert.ThrowsAsync<ConfigurationDependencyException>(
+                    testCode: retrieveAllConfigurationsTask.AsTask);
+
+            this.storageBrokerMock.Verify(broker => 
+                broker.SelectAllConfigurationsAsync(), 
+                    Times.Once());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCriticalAsync(It.Is(SameExceptionAs(
+                    expectedConfigurationDependencyException))), 
+                        Times.Once);
+
+            this.datetimeBrokerMock.Verify(broker => 
+                broker.GetCurrentDateTimeOffsetAsync(), 
+                    Times.Never);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.datetimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
