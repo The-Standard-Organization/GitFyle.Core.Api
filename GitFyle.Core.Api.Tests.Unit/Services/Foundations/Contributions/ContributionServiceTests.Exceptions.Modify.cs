461b1c707d5e1f0da1e82740ecf839800037ae46
slimahmad6 <slimahmad6@outlook.com>
2024-09-18 15:03:21 +0100

    ShouldThrowServiceExceptionOnModifyIfServiceErrorOccursAndLogItAsync -> PASS

--- GitFyle.Core.Api.Tests.Unit/Services/Foundations/Contributions/ContributionServiceTests.Exceptions.Modify.cs
+++ GitFyle.Core.Api.Tests.Unit/Services/Foundations/Contributions/ContributionServiceTests.Exceptions.Modify.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 using FluentAssertions;
 using GitFyle.Core.Api.Models.Foundations.Contributions;
 using GitFyle.Core.Api.Models.Foundations.Contributions.Exceptions;
+using GitFyle.Core.Api.Models.Foundations.Contributions.Exceptions;
+using GitFyle.Core.Api.Models.Foundations.Contributions;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Moq;
@@ -185,6 +187,69 @@ namespace GitFyle.Core.Api.Tests.Unit.Services.Foundations.Contributions
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowServiceExceptionOnModifyIfServiceErrorOccursAndLogItAsync()
+        {
+            // given
+            int minutesInPast = GetRandomNegativeNumber();
+            DateTimeOffset randomDateTimeOffset = GetRandomDateTimeOffset();
+
+            Contribution randomContribution =
+                CreateRandomContribution(randomDateTimeOffset);
+
+            randomContribution.CreatedDate =
+                randomDateTimeOffset.AddMinutes(minutesInPast);
+
+            var serviceException = new Exception();
+
+            var failedServiceContributionException =
+                new FailedServiceContributionException(
+                    message: "Failed service contribution error occurred, contact support.",
+                    innerException: serviceException);
+
+            var expectedContributionServiceException =
+                new ContributionServiceException(
+                    message: "Service error occurred, contact support.",
+                    innerException: failedServiceContributionException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectContributionByIdAsync(randomContribution.Id))
+                    .ThrowsAsync(serviceException);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTimeOffsetAsync())
+                    .ReturnsAsync(randomDateTimeOffset);
+
+            // when
+            ValueTask<Contribution> modifyContributionTask =
+                this.contributionService.ModifyContributionAsync(randomContribution);
+
+            ContributionServiceException actualContributionServiceException =
+                await Assert.ThrowsAsync<ContributionServiceException>(
+                    modifyContributionTask.AsTask);
+
+            // then
+            actualContributionServiceException.Should().BeEquivalentTo(
+                expectedContributionServiceException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectContributionByIdAsync(randomContribution.Id),
+                    Times.Once());
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffsetAsync(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedContributionServiceException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
         }
     }
 }
