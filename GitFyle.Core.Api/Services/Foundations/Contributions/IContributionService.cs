6b22d42c4cb3bc4da547fcbf10d12499d631d708
slimahmad6 <slimahmad6@outlook.com>
2024-09-18 16:05:12 +0100

    ShouldRemoveContributionByIdAsync -> FAIL

--- GitFyle.Core.Api/Services/Foundations/Contributions/IContributionService.cs
+++ GitFyle.Core.Api/Services/Foundations/Contributions/IContributionService.cs
@@ -2,13 +2,15 @@
 // Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
 // ----------------------------------------------------------------------------------
 
-using GitFyle.Core.Api.Models.Foundations.Contributions;
+using System;
 using System.Threading.Tasks;
+using GitFyle.Core.Api.Models.Foundations.Contributions;
 
 namespace GitFyle.Core.Api.Services.Foundations.Contributions
 {
     public interface IContributionService
     {
         ValueTask<Contribution> AddContributionAsync(Contribution contribution);
+        ValueTask<Contribution> RemoveContributionByIdAsync(Guid contributionId);
     }
 }
