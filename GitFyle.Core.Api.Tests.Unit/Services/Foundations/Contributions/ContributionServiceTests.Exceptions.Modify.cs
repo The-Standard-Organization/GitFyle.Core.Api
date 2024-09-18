bcb1f4293f73270934a97fa62468c7575c303c91
slimahmad6 <slimahmad6@outlook.com>
2024-09-18 12:18:37 +0100

    ShouldThrowCriticalDependencyExceptionOnModifyIfSqlErrorOccursAndLogItAsync -> PASS

---
+++ GitFyle.Core.Api.Tests.Unit/Services/Foundations/Contributions/ContributionServiceTests.Exceptions.Modify.cs
@@ -0,0 +1,73 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using GitFyle.Core.Api.Models.Foundations.Contributions;
+using GitFyle.Core.Api.Models.Foundations.Contributions.Exceptions;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+
+namespace GitFyle.Core.Api.Tests.Unit.Services.Foundations.Contributions
+{
+    public partial class ContributionServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowCriticalDependencyExceptionOnModifyIfSqlErrorOccursAndLogItAsync()
+        {
+            // given
+            Contribution someContribution = CreateRandomContribution();
+            SqlException sqlException = CreateSqlException();
+
+            var failedContributionStorageException =
+                new FailedStorageContributionException(
+                    message: "Failed contribution storage error occurred, contact support.",
+                        innerException: sqlException);
+
+            var expectedContributionDependencyException =
+                new ContributionDependencyException(
+                    message: "Contribution dependency error occurred, contact support.",
+                        innerException: failedContributionStorageException);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTimeOffsetAsync())
+                    .ThrowsAsync(sqlException);
+
+            // when
+            ValueTask<Contribution> modifyContributionTask =
+                this.contributionService.ModifyContributionAsync(someContribution);
+
+            ContributionDependencyException actualContributionDependencyException =
+                await Assert.ThrowsAsync<ContributionDependencyException>(
+                    modifyContributionTask.AsTask);
+
+            // then
+            actualContributionDependencyException.Should().BeEquivalentTo(
+                expectedContributionDependencyException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffsetAsync(),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectContributionByIdAsync(someContribution.Id),
+                    Times.Never);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCriticalAsync(It.Is(SameExceptionAs(
+                    expectedContributionDependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateContributionAsync(someContribution),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
