80873ebd9f7a7c6c282ae2a03df41581ac007cc1
Hassan Habib <hassanhabib@live.com>
2024-09-21 11:22:56 -0700

    ShouldReturnCreatedOnPostAsync -> PASS

---
+++ GitFyle.Core.Api.Tests.Unit/Controllers/Sources/SourcesControllerTests.Logic.Post.cs
@@ -0,0 +1,57 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Force.DeepCloner;
+using GitFyle.Core.Api.Controllers;
+using GitFyle.Core.Api.Models.Foundations.Sources;
+using GitFyle.Core.Api.Services.Foundations.Sources;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using RESTFulSense.Clients.Extensions;
+using RESTFulSense.Models;
+using Tynamix.ObjectFiller;
+
+namespace GitFyle.Core.Api.Tests.Unit.Controllers.Sources
+{
+    public partial class SourcesControllerTests
+    {
+        [Fact]
+        public async Task ShouldReturnCreatedOnPostAsync()
+        {
+            // given
+            Source randomSource = CreateRandomSource();
+            Source inputSource = randomSource;
+            Source addedSource = inputSource;
+            Source expectedSource = addedSource.DeepClone();
+
+            var expectedObjectResult =
+                new CreatedObjectResult(expectedSource);
+
+            var expectedActionResult =
+                new ActionResult<Source>(expectedObjectResult);
+
+            this.sourceServiceMock.Setup(service =>
+                service.AddSourceAsync(inputSource))
+                    .ReturnsAsync(addedSource);
+
+            // when
+            ActionResult<Source> actualActionResult =
+                await this.sourcesController.PostSourceAsync(
+                    inputSource);
+
+            // then
+            actualActionResult.ShouldBeEquivalentTo(
+                expectedActionResult);
+
+            this.sourceServiceMock.Verify(service =>
+                service.AddSourceAsync(inputSource),
+                    Times.Once);
+
+            this.sourceServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ GitFyle.Core.Api.Tests.Unit/Controllers/Sources/SourcesControllerTests.cs
@@ -0,0 +1,45 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System;
+using GitFyle.Core.Api.Controllers;
+using GitFyle.Core.Api.Models.Foundations.Sources;
+using GitFyle.Core.Api.Services.Foundations.Sources;
+using Moq;
+using Tynamix.ObjectFiller;
+
+namespace GitFyle.Core.Api.Tests.Unit.Controllers.Sources
+{
+    public partial class SourcesControllerTests
+    {
+        private readonly Mock<ISourceService> sourceServiceMock;
+        private readonly SourcesController sourcesController;
+
+        public SourcesControllerTests()
+        {
+            this.sourceServiceMock = new Mock<ISourceService>();
+
+            this.sourcesController = new SourcesController(
+                sourceService: this.sourceServiceMock.Object);
+        }
+
+        private static Source CreateRandomSource() =>
+            CreateSourceFiller().Create();
+
+        private static DateTimeOffset GetRandomDateTimeOffset() =>
+            new DateTimeRange(earliestDate: new DateTime()).GetValue();
+
+        private static Filler<Source> CreateSourceFiller()
+        {
+            var filler = new Filler<Source>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(GetRandomDateTimeOffset)
+                .OnProperty(source => source.Contributors).IgnoreIt()
+                .OnProperty(source => source.Repositories).IgnoreIt();
+
+            return filler;
+        }
+    }
+}
