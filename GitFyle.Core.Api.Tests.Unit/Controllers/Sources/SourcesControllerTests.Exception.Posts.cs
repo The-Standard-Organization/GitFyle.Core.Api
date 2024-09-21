63d9cd07b61dc86b38d3658a9af28f10aafd5e8c
Hassan Habib <hassanhabib@live.com>
2024-09-21 11:46:31 -0700

    ShouldReturnBadRequestOnPostIfValidationErrorOccurredAsync -> PASS

---
+++ GitFyle.Core.Api.Tests.Unit/Controllers/Sources/SourcesControllerTests.Exception.Posts.cs
@@ -0,0 +1,49 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using GitFyle.Core.Api.Models.Foundations.Sources;
+using GitFyle.Core.Api.Models.Foundations.Sources.Exceptions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using RESTFulSense.Clients.Extensions;
+using Xeptions;
+
+namespace GitFyle.Core.Api.Tests.Unit.Controllers.Sources
+{
+    public partial class SourcesControllerTests
+    {
+        [Theory]
+        [MemberData(nameof(ValidationExceptions))]
+        public async Task ShouldReturnBadRequestOnPostIfValidationErrorOccurredAsync(
+            Xeption validationException)
+        {
+            // given
+            Source someSource = CreateRandomSource();
+
+            BadRequestObjectResult expectedBadRequestObjectResult =
+                BadRequest(validationException.InnerException);
+
+            var expectedActionResult =
+                new ActionResult<Source>(expectedBadRequestObjectResult);
+
+            this.sourceServiceMock.Setup(service =>
+                service.AddSourceAsync(It.IsAny<Source>()))
+                    .ThrowsAsync(validationException);
+
+            // when
+            ActionResult<Source> actualActionResult =
+                await this.sourcesController.PostSourceAsync(someSource);
+
+            // then
+            actualActionResult.ShouldBeEquivalentTo(expectedActionResult);
+
+            this.sourceServiceMock.Verify(service =>
+                service.AddSourceAsync(It.IsAny<Source>()),
+                    Times.Once);
+
+            this.sourceServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}
--- GitFyle.Core.Api.Tests.Unit/Controllers/Sources/SourcesControllerTests.cs
+++ GitFyle.Core.Api.Tests.Unit/Controllers/Sources/SourcesControllerTests.cs
@@ -8,10 +8,14 @@ using GitFyle.Core.Api.Models.Foundations.Sources;
 using GitFyle.Core.Api.Services.Foundations.Sources;
 using Moq;
 using Tynamix.ObjectFiller;
+using RESTFulSense.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Xeptions;
+using GitFyle.Core.Api.Models.Foundations.Sources.Exceptions;
 
 namespace GitFyle.Core.Api.Tests.Unit.Controllers.Sources
 {
-    public partial class SourcesControllerTests
+    public partial class SourcesControllerTests : RESTFulController
     {
         private readonly Mock<ISourceService> sourceServiceMock;
         private readonly SourcesController sourcesController;
@@ -24,8 +28,28 @@ namespace GitFyle.Core.Api.Tests.Unit.Controllers.Sources
                 sourceService: this.sourceServiceMock.Object);
         }
 
+        public static TheoryData ValidationExceptions()
+        {
+            var someInnerException = new Xeption();
+            string someMessage = CreateRandomString();
+
+            return new TheoryData<Xeption>
+            {
+                new SourceValidationException(
+                    message: someMessage,
+                    innerException: someInnerException),
+
+                new SourceDependencyValidationException(
+                    message: someMessage,
+                    innerException: someInnerException)
+            };
+        }
+
         private static Source CreateRandomSource() =>
             CreateSourceFiller().Create();
+
+        private static string CreateRandomString() =>
+            new MnemonicString().GetValue();    
 
         private static DateTimeOffset GetRandomDateTimeOffset() =>
             new DateTimeRange(earliestDate: new DateTime()).GetValue();
