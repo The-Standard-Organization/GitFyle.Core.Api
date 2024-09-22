3ab9939caef25a7c6d7601c9d81c68fb14fb07b9
Pratik Kaje <kajepratik@gmail.com>
2024-09-22 08:14:23 +0400

    ShouldThrowValidationExceptionOnRetrieveByIdIfIdIsInvalidAndLogItAsync => FAIL

---
+++ GitFyle.Core.Api.Tests.Unit/Services/Foundations/Configurations/ConfigurationServiceTests.Validations.RetrieveById.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using GitFyle.Core.Api.Models.Foundations.Configurations;
+using GitFyle.Core.Api.Models.Foundations.Configurations.Exceptions;
+using Moq;
+
+namespace GitFyle.Core.Api.Tests.Unit.Services.Foundations.Configurations
+{
+    public partial class ConfigurationServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnRetrieveByIdIfIdIsInvalidAndLogItAsync()
+        {
+            // given
+            Guid id = Guid.Empty;
+
+            var invalidConfigurationException = 
+                new InvalidConfigurationException(
+                    message: "Configuration is invalid. fix the errors and try again.");
+
+            invalidConfigurationException.AddData(
+                key: nameof(Configuration.Id), 
+                values: "Id is invalid.");
+
+            var expectedConfigurationValidationException =
+                new ConfigurationValidationException(
+                    message: "Configuration validation error occurred, fix errors and try again.", 
+                    innerException: invalidConfigurationException);
+
+            // when
+            ValueTask<Configuration> retrieveConfigurationByIdTask = 
+                this.configurationService.RetrieveConfigurationByIdAsync(id);
+
+            ConfigurationValidationException actualConfigurationValidationException = 
+                await Assert.ThrowsAsync<ConfigurationValidationException>(
+                    retrieveConfigurationByIdTask.AsTask);
+            // then
+            actualConfigurationValidationException.Should().BeEquivalentTo(
+                expectedConfigurationValidationException);
+
+            this.loggingBrokerMock.Verify(broker => 
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedConfigurationValidationException))), 
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker => 
+                broker.SelectConfigurationByIdAsync(It.IsAny<Guid>()),
+                        Times.Never);
+
+            this.datetimeBrokerMock.Verify(broker => 
+                broker.GetCurrentDateTimeOffsetAsync(), 
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.datetimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
