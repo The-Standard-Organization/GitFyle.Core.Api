beff89a783a933eda06ebd033034b388238e1b9d
slimahmad6 <slimahmad6@outlook.com>
2024-09-26 09:46:27 +0100

    CODERUB: Code cleanup

--- GitFyle.Core.Api/Services/Foundations/Contributions/ContributionService.cs
+++ GitFyle.Core.Api/Services/Foundations/Contributions/ContributionService.cs
@@ -2,13 +2,12 @@
 // Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
 // ----------------------------------------------------------------------------------
 
-using System.Linq;
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 using GitFyle.Core.Api.Brokers.DateTimes;
 using GitFyle.Core.Api.Brokers.Loggings;
 using GitFyle.Core.Api.Brokers.Storages;
-using GitFyle.Core.Api.Models.Foundations.Contributions;
 using GitFyle.Core.Api.Models.Foundations.Contributions;
 
 namespace GitFyle.Core.Api.Services.Foundations.Contributions
@@ -39,7 +38,6 @@ namespace GitFyle.Core.Api.Services.Foundations.Contributions
         
         public ValueTask<IQueryable<Contribution>> RetrieveAllContributionsAsync() =>
         TryCatch(async () => await this.storageBroker.SelectAllContributionsAsync());
-        });
 
         public ValueTask<Contribution> RemoveContributionByIdAsync(Guid contributionId) =>
         TryCatch(async () =>
