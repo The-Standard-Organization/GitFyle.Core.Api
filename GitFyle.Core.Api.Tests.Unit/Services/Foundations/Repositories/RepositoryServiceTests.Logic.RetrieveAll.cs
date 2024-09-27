d946f3978139df1778afc578e2574af58e70e640
Greg Hays <glhays.dev@gmail.com>
2024-09-27 10:54:10 -0700

    ShouldRetrieveAllRepositoriesAsync => FAIL
    Closes: #111

---
+++ GitFyle.Core.Api.Tests.Unit/Services/Foundations/Repositories/RepositoryServiceTests.Logic.RetrieveAll.cs
@@ -0,0 +1,43 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using GitFyle.Core.Api.Models.Foundations.Repositories;
+using Moq;
+
+namespace GitFyle.Core.Api.Tests.Unit.Services.Foundations.Repositories
+{
+    public partial class RepositoryServiceTests
+    {
+        [Fact]
+        public async Task ShouldRetrieveAllRepositoriesAsync()
+        {
+            // given
+            IQueryable<Repository> randomRepositories = CreateRandomRepositories();
+            IQueryable<Repository> storageRepositories = randomRepositories;
+            IQueryable<Repository> expectedRepositories = storageRepositories;
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllRepositoriesAsync())
+                    .ReturnsAsync(storageRepositories);
+
+            // when
+            IQueryable<Repository> actualRepositories =
+                await this.repositoryService.RetrieveAllRepositoriesAsync();
+
+            // then
+            actualRepositories.Should().BeEquivalentTo(expectedRepositories);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllRepositoriesAsync(),
+                    Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
