fc6b4aba7238f9a5d31889ad54e835c6ca19ea2a
Greg Hays <glhays.dev@gmail.com>
2024-09-29 10:46:33 -0700

    ShouldThrowValidationExceptionOnRetrieveByIdWhenRepositoryIdIsInvalidAndLogItAsync => FAIL

---
+++ GitFyle.Core.Api.Tests.Unit/Services/Foundations/Repositories/RepositoryServiceTests.Validations.RetrieveById.cs
@@ -0,0 +1,65 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using GitFyle.Core.Api.Models.Foundations.Repositories;
+using GitFyle.Core.Api.Models.Foundations.Repositories.Exceptions;
+using Moq;
+
+namespace GitFyle.Core.Api.Tests.Unit.Services.Foundations.Repositories
+{
+    public partial class RepositoryServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnRetrieveByIdWhenRepositoryIdIsInvalidAndLogItAsync()
+        {
+            // given
+            var invalidRepositoryId = Guid.Empty;
+
+            var invalidRepositoryException =
+                new InvalidRepositoryException(
+                    message: "Repository is invalid, fix the errors and try again.");
+
+            invalidRepositoryException.AddData(
+                key: nameof(Repository.Id),
+                values: "Id is invalid");
+
+            var expectedRepositoryValidationException =
+                new RepositoryValidationException(
+                    message: "Repository validation error occurred, fix errors and try again.",
+                    innerException: invalidRepositoryException);
+
+            // when
+            ValueTask<Repository> retrieveRepositoryByIdTask =
+                this.repositoryService.RetrieveRepositoryByIdAsync(invalidRepositoryId);
+
+            RepositoryValidationException actualRepositoryValidationException =
+                await Assert.ThrowsAsync<RepositoryValidationException>(
+                    retrieveRepositoryByIdTask.AsTask);
+
+            // then
+            actualRepositoryValidationException.Should().BeEquivalentTo(
+                expectedRepositoryValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedRepositoryValidationException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectRepositoryByIdAsync(It.IsAny<Guid>()),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffsetAsync(),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
