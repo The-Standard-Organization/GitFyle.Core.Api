0a6fdbeded472e0b766e986404bb9a8a5b7f93ca
Pratik Kaje <kajepratik@gmail.com>
2024-09-30 20:37:39 +0400

    CODE RUB: Added missing banner

--- GitFyle.Core.Api.Tests.Unit/Services/Foundations/Configurations/ConfigurationServiceTests.Validations.Modify.cs
+++ GitFyle.Core.Api.Tests.Unit/Services/Foundations/Configurations/ConfigurationServiceTests.Validations.Modify.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System;
 using System.Threading.Tasks;
 using FluentAssertions;
 using Force.DeepCloner;
