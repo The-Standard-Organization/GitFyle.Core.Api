0137d628ede965c0610d797c2688be41beb8c774
Christo du Toit <christo@dutoit.co.uk>
2024-10-03 20:01:58 +0100

    ShouldReturnInternalServerErrorOnGetByIdIfServerErrorOccurredAsync -> FAIL

--- GitFyle.Core.Api.Tests.Unit/Controllers/Sources/SourcesControllerTests.Exception.GetById.cs
+++ GitFyle.Core.Api.Tests.Unit/Controllers/Sources/SourcesControllerTests.Exception.GetById.cs
@@ -8,6 +8,7 @@ using GitFyle.Core.Api.Models.Foundations.Sources;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using RESTFulSense.Clients.Extensions;
+using RESTFulSense.Models;
 using Xeptions;
 
 namespace GitFyle.Core.Api.Tests.Unit.Controllers.Sources
@@ -23,6 +24,38 @@ namespace GitFyle.Core.Api.Tests.Unit.Controllers.Sources
 
             BadRequestObjectResult expectedBadRequestObjectResult =
                 BadRequest(validationException.InnerException);
+
+            var expectedActionResult =
+                new ActionResult<Source>(expectedBadRequestObjectResult);
+
+            this.sourceServiceMock.Setup(service =>
+                service.RetrieveSourceByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(validationException);
+
+            // when
+            ActionResult<Source> actualActionResult =
+                await this.sourcesController.GetSourceByIdAsync(someId);
+
+            // then
+            actualActionResult.ShouldBeEquivalentTo(expectedActionResult);
+
+            this.sourceServiceMock.Verify(service =>
+                service.RetrieveSourceByIdAsync(It.IsAny<Guid>()),
+                    Times.Once);
+
+            this.sourceServiceMock.VerifyNoOtherCalls();
+        }
+
+        [Theory]
+        [MemberData(nameof(ServerExceptions))]
+        public async Task ShouldReturnInternalServerErrorOnGetByIdIfServerErrorOccurredAsync(
+            Xeption validationException)
+        {
+            // given
+            Guid someId = Guid.NewGuid();
+
+            InternalServerErrorObjectResult expectedBadRequestObjectResult =
+                InternalServerError(validationException);
 
             var expectedActionResult =
                 new ActionResult<Source>(expectedBadRequestObjectResult);
