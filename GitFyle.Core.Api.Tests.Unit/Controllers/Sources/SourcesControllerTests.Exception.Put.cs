345974fcb8015ca1b82eeb3e53f1af07a558489c
Christo du Toit <christo@dutoit.co.uk>
2024-10-03 20:15:56 +0100

    ShouldReturnBadRequestOnPutIfValidationErrorOccurredAsync -> FAIL

---
+++ GitFyle.Core.Api.Tests.Unit/Controllers/Sources/SourcesControllerTests.Exception.Put.cs
@@ -0,0 +1,47 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using GitFyle.Core.Api.Models.Foundations.Sources;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using RESTFulSense.Clients.Extensions;
+using Xeptions;
+
+namespace GitFyle.Core.Api.Tests.Unit.Controllers.Sources
+{
+    public partial class SourcesControllerTests
+    {
+        [Theory]
+        [MemberData(nameof(ValidationExceptions))]
+        public async Task ShouldReturnBadRequestOnPutIfValidationErrorOccurredAsync(Xeption validationException)
+        {
+            // given
+            Source someSource = CreateRandomSource();
+
+            BadRequestObjectResult expectedBadRequestObjectResult =
+                BadRequest(validationException.InnerException);
+
+            var expectedActionResult =
+                new ActionResult<Source>(expectedBadRequestObjectResult);
+
+            this.sourceServiceMock.Setup(service =>
+                service.ModifySourceAsync(It.IsAny<Source>()))
+                    .ThrowsAsync(validationException);
+
+            // when
+            ActionResult<Source> actualActionResult =
+                await this.sourcesController.PutSourceAsync(someSource);
+
+            // then
+            actualActionResult.ShouldBeEquivalentTo(expectedActionResult);
+
+            this.sourceServiceMock.Verify(service =>
+                service.ModifySourceAsync(It.IsAny<Source>()),
+                    Times.Once);
+
+            this.sourceServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}
