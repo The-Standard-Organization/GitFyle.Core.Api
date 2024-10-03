10e6873e0ce5abadd5f9b31ef817b97699dd9cc7
Christo du Toit <christo@dutoit.co.uk>
2024-10-03 19:58:19 +0100

    ShouldReturnBadRequestOnGetByIdIfValidationErrorOccurredAsync -> FAIL

---
+++ GitFyle.Core.Api.Tests.Unit/Controllers/Sources/SourcesControllerTests.Exception.GetById.cs
@@ -0,0 +1,48 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using GitFyle.Core.Api.Models.Foundations.Sources;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using RESTFulSense.Clients.Extensions;
+using Xeptions;
+
+namespace GitFyle.Core.Api.Tests.Unit.Controllers.Sources
+{
+    public partial class SourcesControllerTests
+    {
+        [Theory]
+        [MemberData(nameof(ValidationExceptions))]
+        public async Task ShouldReturnBadRequestOnGetByIdIfValidationErrorOccurredAsync(Xeption validationException)
+        {
+            // given
+            Guid someId = Guid.NewGuid();
+
+            BadRequestObjectResult expectedBadRequestObjectResult =
+                BadRequest(validationException.InnerException);
+
+            var expectedActionResult =
+                new ActionResult<Source>(expectedBadRequestObjectResult);
+
+            this.sourceServiceMock.Setup(service =>
+                service.RetrieveSourceByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(validationException);
+
+            // when
+            ActionResult<Source> actualActionResult =
+                await this.sourcesController.GetSourceByIdAsync(someId);
+
+            // then
+            actualActionResult.ShouldBeEquivalentTo(expectedActionResult);
+
+            this.sourceServiceMock.Verify(service =>
+                service.RetrieveSourceByIdAsync(It.IsAny<Guid>()),
+                    Times.Once);
+
+            this.sourceServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}
