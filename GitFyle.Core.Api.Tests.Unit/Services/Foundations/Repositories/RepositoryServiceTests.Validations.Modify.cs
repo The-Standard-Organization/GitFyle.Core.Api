64de87bd3c62f1df558e47e4547497af67447c0b
slimahmad6 <slimahmad6@outlook.com>
2024-10-05 22:55:49 +0100

    ShouldThrowValidationExceptionOnModifyIfRepositoryIsNullAndLogItAsync -> PASS

---
+++ GitFyle.Core.Api.Tests.Unit/Services/Foundations/Repositories/RepositoryServiceTests.Validations.Modify.cs
@@ -0,0 +1,57 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Force.DeepCloner;
+using GitFyle.Core.Api.Models.Foundations.Repositories;
+using GitFyle.Core.Api.Models.Foundations.Repositories.Exceptions;
+using Moq;
+
+namespace GitFyle.Core.Api.Tests.Unit.Services.Foundations.Repositories
+{
+    public partial class RepositoryServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnModifyIfRepositoryIsNullAndLogItAsync()
+        {
+            // given
+            Repository nullRepository = null;
+
+            var nullRepositoryException =
+                new NullRepositoryException(message: "Repository is null");
+
+            var expectedRepositoryValidationException =
+                new RepositoryValidationException(
+                    message: "Repository validation error occurred, fix errors and try again.",
+                    innerException: nullRepositoryException);
+
+            // when
+            ValueTask<Repository> addRepositoryTask =
+                this.repositoryService.AddRepositoryAsync(nullRepository);
+
+            RepositoryValidationException actualRepositoryValidationException =
+                await Assert.ThrowsAsync<RepositoryValidationException>(
+                    testCode: addRepositoryTask.AsTask);
+
+            // then
+            actualRepositoryValidationException.Should().BeEquivalentTo(
+                expectedRepositoryValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(
+                    SameExceptionAs(expectedRepositoryValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertRepositoryAsync(It.IsAny<Repository>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
