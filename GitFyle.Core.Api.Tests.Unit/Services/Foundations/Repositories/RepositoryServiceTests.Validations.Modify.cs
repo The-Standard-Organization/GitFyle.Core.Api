43d2628dade4a32346e82bf95652f0e787871c31
slimahmad6 <slimahmad6@outlook.com>
2024-10-06 08:18:33 +0100

    ShouldThrowValidationExceptionOnModifyIfRepositoryIsInvalidAndLogItAsync -> PASS

--- GitFyle.Core.Api.Tests.Unit/Services/Foundations/Repositories/RepositoryServiceTests.Validations.Modify.cs
+++ GitFyle.Core.Api.Tests.Unit/Services/Foundations/Repositories/RepositoryServiceTests.Validations.Modify.cs
@@ -53,5 +53,130 @@ namespace GitFyle.Core.Api.Tests.Unit.Services.Foundations.Repositories
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnModifyIfRepositoryIsInvalidAndLogItAsync(
+                string invalidString)
+        {
+            // given
+            var invalidRepository = new Repository
+            {
+                Id = Guid.Empty,
+                Name = invalidString,
+                Owner = invalidString,
+                ExternalId = invalidString,
+                SourceId = Guid.Empty,
+                Token = invalidString,
+                TokenExpireAt = default,
+                Description = invalidString,
+                ExternalCreatedAt = default,
+                ExternalUpdatedAt = default,
+                CreatedBy = invalidString,
+                CreatedDate = default,
+                UpdatedBy = invalidString,
+                UpdatedDate = default,
+            };
+
+            var invalidRepositoryException = new InvalidRepositoryException(
+                message: "Repository is invalid, fix the errors and try again.");
+
+            invalidRepositoryException.AddData(
+                key: nameof(Repository.Id),
+                values: "Id is invalid");
+
+            invalidRepositoryException.AddData(
+                key: nameof(Repository.Name),
+                values: "Text is required");
+
+            invalidRepositoryException.AddData(
+                key: nameof(Repository.Owner),
+                values: "Text is required");
+
+            invalidRepositoryException.AddData(
+                key: nameof(Repository.ExternalId),
+                values: "Text is required");
+
+            invalidRepositoryException.AddData(
+                key: nameof(Repository.SourceId),
+                values: "Id is invalid");
+
+            invalidRepositoryException.AddData(
+                key: nameof(Repository.Token),
+                values: "Text is required");
+
+            invalidRepositoryException.AddData(
+                key: nameof(Repository.TokenExpireAt),
+                values: "Date is invalid");
+
+            invalidRepositoryException.AddData(
+                key: nameof(Repository.Description),
+                values: "Text is required");
+
+            invalidRepositoryException.AddData(
+                key: nameof(Repository.ExternalCreatedAt),
+                values: "Date is invalid");
+
+            invalidRepositoryException.AddData(
+                key: nameof(Repository.ExternalUpdatedAt),
+                values: "Date is invalid");
+
+            invalidRepositoryException.AddData(
+               key: nameof(Repository.CreatedBy),
+               values: "Text is required");
+
+            invalidRepositoryException.AddData(
+                key: nameof(Repository.UpdatedBy),
+                values: "Text is required");
+
+            invalidRepositoryException.AddData(
+                key: nameof(Repository.CreatedDate),
+                values: "Date is invalid");
+
+            invalidRepositoryException.AddData(
+                key: nameof(Repository.UpdatedDate),
+                values:
+                    new[]
+                    {
+                        "Date is invalid",
+                        $"Date is the same as {nameof(Repository.CreatedDate)}"
+                    });
+
+            var expectedRepositoryValidationException =
+                new RepositoryValidationException(
+                    message: "Repository validation error occurred, fix errors and try again.",
+                    innerException: invalidRepositoryException);
+
+            // when
+            ValueTask<Repository> modifyRepositoryTask =
+                this.repositoryService.ModifyRepositoryAsync(invalidRepository);
+
+            RepositoryValidationException actualRepositoryValidationException =
+                await Assert.ThrowsAsync<RepositoryValidationException>(
+                    testCode: modifyRepositoryTask.AsTask);
+
+            // then
+            actualRepositoryValidationException.Should().BeEquivalentTo(
+                expectedRepositoryValidationException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffsetAsync(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(
+                    SameExceptionAs(expectedRepositoryValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertRepositoryAsync(It.IsAny<Repository>()),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
