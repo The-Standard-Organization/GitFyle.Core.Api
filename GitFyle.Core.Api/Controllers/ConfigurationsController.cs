d3549ad9289336136f791231a816bd1c74697d3e
Pratik Kaje <kajepratik@gmail.com>
2024-10-07 19:31:04 +0400

    CODE RUB: Added HttpPUT for PutSourceAsync

---
+++ GitFyle.Core.Api/Controllers/ConfigurationsController.cs
@@ -0,0 +1,52 @@
+﻿using System.Threading.Tasks;
+using GitFyle.Core.Api.Models.Foundations.Configurations;
+using GitFyle.Core.Api.Models.Foundations.Configurations.Exceptions;
+using GitFyle.Core.Api.Services.Foundations.Configurations;
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+
+namespace GitFyle.Core.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ConfigurationsController : RESTFulController
+    {
+        private readonly IConfigurationService configurationService;
+
+        public ConfigurationsController(IConfigurationService configurationService) =>
+            this.configurationService = configurationService;
+
+        [HttpPost]
+        public async ValueTask<ActionResult<Configuration>> PostConfigurationAsync(Configuration configuration)
+        {
+            try
+            {
+                Configuration addedConfiguration =
+                    await this.configurationService.AddConfigurationAsync(configuration);
+
+                return Created(configuration);
+            }
+            catch (ConfigurationValidationException configurationValidationException)
+            {
+                return BadRequest(configurationValidationException.InnerException);
+            }
+            catch (ConfigurationDependencyValidationException configurationDependencyValidationException)
+                when (configurationDependencyValidationException.InnerException is AlreadyExistsConfigurationException)
+            {
+                return Conflict(configurationDependencyValidationException.InnerException);
+            }
+            catch (ConfigurationDependencyValidationException configurationDependencyValidationException)
+            {
+                return BadRequest(configurationDependencyValidationException);
+            }
+            catch (ConfigurationDependencyException configurationDependencyException)
+            {
+                return InternalServerError(configurationDependencyException.InnerException);
+            }
+            catch (ConfigurationServiceException configurationServiceException) 
+            {
+                return InternalServerError(configurationServiceException);
+            }
+        }
+    }
+}
