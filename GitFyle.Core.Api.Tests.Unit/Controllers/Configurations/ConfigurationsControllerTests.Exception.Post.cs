4a976d337eef5e80bdab1fa113a98d7986135f4c
Pratik Kaje <kajepratik@gmail.com>
2024-10-12 23:24:11 +0400

    ShouldReturnConflictOnPostIfAlreadyExistsConfigurationErrorOccurredAsync => PASS

--- GitFyle.Core.Api.Tests.Unit/Controllers/Configurations/ConfigurationsControllerTests.Exception.Post.cs
+++ GitFyle.Core.Api.Tests.Unit/Controllers/Configurations/ConfigurationsControllerTests.Exception.Post.cs
@@ -5,6 +5,8 @@ using Moq;
 using Xeptions;
 using RESTFulSense.Clients.Extensions;
 using RESTFulSense.Models;
+using System;
+using GitFyle.Core.Api.Models.Foundations.Configurations.Exceptions;
 
 namespace GitFyle.Core.Api.Tests.Unit.Controllers.Configurations
 {
@@ -58,6 +60,49 @@ namespace GitFyle.Core.Api.Tests.Unit.Controllers.Configurations
             this.configurationServiceMock.Setup(service =>
                 service.AddConfigurationAsync(It.IsAny<Configuration>()))
                     .ThrowsAsync(serverException);
+
+            // when
+            ActionResult<Configuration> actualActionResult =
+                await this.configurationsController.PostConfigurationAsync(someConfiguration);
+
+            // then
+            actualActionResult.ShouldBeEquivalentTo(expectedActionResult);
+
+            this.configurationServiceMock.Verify(service =>
+                service.AddConfigurationAsync(It.IsAny<Configuration>()),
+                    Times.Once);
+
+            this.configurationServiceMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldReturnConflictOnPostIfAlreadyExistsConfigurationErrorOccurredAsync()
+        {
+            // given
+            Configuration someConfiguration = CreateRandomConfiguration();
+            var someInnerException = new Exception();
+            string someMessage = GetRandomString();
+
+            var alreadyExistsConfigurationException =
+                new AlreadyExistsConfigurationException(
+                    message: someMessage,
+                    innerException: someInnerException,
+                    data: someInnerException.Data);
+
+            var configurationDependencyValidationException =
+                new ConfigurationDependencyValidationException(
+                    message: someMessage,
+                    innerException: alreadyExistsConfigurationException);
+
+            ConflictObjectResult expectedConflictObjectResult =
+                Conflict(alreadyExistsConfigurationException);
+
+            var expectedActionResult =
+                new ActionResult<Configuration>(expectedConflictObjectResult);
+
+            this.configurationServiceMock.Setup(service =>
+                service.AddConfigurationAsync(It.IsAny<Configuration>()))
+                    .ThrowsAsync(configurationDependencyValidationException);
 
             // when
             ActionResult<Configuration> actualActionResult =
