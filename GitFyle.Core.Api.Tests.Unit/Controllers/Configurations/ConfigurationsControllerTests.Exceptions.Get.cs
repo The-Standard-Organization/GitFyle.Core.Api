1f7a836f1cb2cd6abbd17f1752041af9933a59e0
Pratik Kaje <kajepratik@gmail.com>
2024-10-13 20:00:16 +0400

    ShouldReturnInternalServerErrorOnGetIfServerErrorOccurredAsync => FAIL

---
+++ GitFyle.Core.Api.Tests.Unit/Controllers/Configurations/ConfigurationsControllerTests.Exceptions.Get.cs
@@ -0,0 +1,51 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GitFyle.Core.Api.Models.Foundations.Configurations;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using RESTFulSense.Clients.Extensions;
+using RESTFulSense.Models;
+using Xeptions;
+
+namespace GitFyle.Core.Api.Tests.Unit.Controllers.Configurations
+{
+    public partial class ConfigurationsControllerTests
+    {
+        [Theory]
+        [MemberData(nameof(ServerExceptions))]
+        public async Task ShouldReturnInternalServerErrorOnGetIfServerErrorOccurredAsync(
+            Xeption serverException)
+        {
+            // given
+            InternalServerErrorObjectResult expectedInternalServerErrorObjectResult =
+                InternalServerError(serverException);
+
+            var expectedActionResult =
+                new ActionResult<IQueryable<Configuration>>(expectedInternalServerErrorObjectResult);
+
+            this.configurationServiceMock.Setup(service =>
+                service.RetrieveAllConfigurationsAsync())
+                    .ThrowsAsync(serverException);
+
+            // when
+            ActionResult<IQueryable<Configuration>> actualActionResult =
+                await this.configurationsController.GetConfigurationsAsync();
+
+            // then
+            actualActionResult.ShouldBeEquivalentTo(expectedActionResult);
+
+            this.configurationServiceMock.Verify(service =>
+                service.RetrieveAllConfigurationsAsync(),
+                    Times.Once);
+
+            this.configurationServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}
