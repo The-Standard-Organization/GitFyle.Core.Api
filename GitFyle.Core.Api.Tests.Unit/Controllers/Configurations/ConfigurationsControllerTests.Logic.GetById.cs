1da967229f9285751dfe90d538681430770eac74
Pratik Kaje <kajepratik@gmail.com>
2024-10-13 19:38:40 +0400

    CODE RUB: Added Missing Banner

--- GitFyle.Core.Api.Tests.Unit/Controllers/Configurations/ConfigurationsControllerTests.Logic.GetById.cs
+++ GitFyle.Core.Api.Tests.Unit/Controllers/Configurations/ConfigurationsControllerTests.Logic.GetById.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System;
 using System.Threading.Tasks;
 using Force.DeepCloner;
 using GitFyle.Core.Api.Models.Foundations.Configurations;
