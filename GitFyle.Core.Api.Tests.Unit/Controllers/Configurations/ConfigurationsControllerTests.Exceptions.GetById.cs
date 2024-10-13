a5e70141dce68de1a12c0e332ced3757058d4e05
Pratik Kaje <kajepratik@gmail.com>
2024-10-13 19:36:43 +0400

    ShouldReturnNotFoundOnGetByIdIfItemDoesNotExistAsync => PASS

--- GitFyle.Core.Api.Tests.Unit/Controllers/Configurations/ConfigurationsControllerTests.Exceptions.GetById.cs
+++ GitFyle.Core.Api.Tests.Unit/Controllers/Configurations/ConfigurationsControllerTests.Exceptions.GetById.cs
@@ -9,6 +9,7 @@ using Moq;
 using RESTFulSense.Clients.Extensions;
 using RESTFulSense.Models;
 using Xeptions;
+using GitFyle.Core.Api.Models.Foundations.Configurations.Exceptions;
 
 namespace GitFyle.Core.Api.Tests.Unit.Controllers.Configurations
 {
@@ -62,6 +63,46 @@ namespace GitFyle.Core.Api.Tests.Unit.Controllers.Configurations
             this.configurationServiceMock.Setup(service =>
                 service.RetrieveConfigurationByIdAsync(It.IsAny<Guid>()))
                     .ThrowsAsync(validationException);
+
+            // when
+            ActionResult<Configuration> actualActionResult =
+                await this.configurationsController.GetConfigurationByIdAsync(someId);
+
+            // then
+            actualActionResult.ShouldBeEquivalentTo(expectedActionResult);
+
+            this.configurationServiceMock.Verify(service =>
+                service.RetrieveConfigurationByIdAsync(It.IsAny<Guid>()),
+                    Times.Once);
+
+            this.configurationServiceMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldReturnNotFoundOnGetByIdIfItemDoesNotExistAsync()
+        {
+            // given
+            Guid someId = Guid.NewGuid();
+            string someMessage = GetRandomString();
+
+            var notFoundConfigurationException =
+                new NotFoundConfigurationException(
+                    message: someMessage);
+
+            var configurationValidationException =
+                new ConfigurationValidationException(
+                    message: someMessage,
+                    innerException: notFoundConfigurationException);
+
+            NotFoundObjectResult expectedNotFoundObjectResult =
+                NotFound(notFoundConfigurationException);
+
+            var expectedActionResult =
+                new ActionResult<Configuration>(expectedNotFoundObjectResult);
+
+            this.configurationServiceMock.Setup(service =>
+                service.RetrieveConfigurationByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(configurationValidationException);
 
             // when
             ActionResult<Configuration> actualActionResult =
