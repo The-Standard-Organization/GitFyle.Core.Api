d97e709149fae2c9fa9a3080c2bcc980999ea950
slimahmad6 <slimahmad6@outlook.com>
2024-10-13 23:18:59 +0100

    ShouldThrowCriticalDependencyExceptionOnAddIfSqlErrorOccurredAndLogItAsync -> PASS

--- GitFyle.Core.Api/Services/Foundations/ContributionTypes/ContributionTypeService.Exceptions.cs
+++ GitFyle.Core.Api/Services/Foundations/ContributionTypes/ContributionTypeService.Exceptions.cs
@@ -5,6 +5,7 @@
 using System.Threading.Tasks;
 using GitFyle.Core.Api.Models.Foundations.ContributionTypes;
 using GitFyle.Core.Api.Models.Foundations.ContributionTypes.Exceptions;
+using Microsoft.Data.SqlClient;
 using Xeptions;
 
 namespace GitFyle.Core.Api.Services.Foundations.ContributionTypes
@@ -27,6 +28,14 @@ namespace GitFyle.Core.Api.Services.Foundations.ContributionTypes
             {
                 throw await CreateAndLogValidationExceptionAsync(invalidContributionTypeException);
             }
+            catch (SqlException sqlException)
+            {
+                var failedStorageContributionTypeException = new FailedStorageContributionTypeException(
+                    message: "Failed storage contributionType error occurred, contact support.",
+                    innerException: sqlException);
+
+                throw await CreateAndLogCriticalDependencyExceptionAsync(failedStorageContributionTypeException);
+            }
         }
 
         private async ValueTask<ContributionTypeValidationException>
@@ -39,6 +48,18 @@ namespace GitFyle.Core.Api.Services.Foundations.ContributionTypes
             await this.loggingBroker.LogErrorAsync(contributionValidationException);
 
             return contributionValidationException;
+        }
+
+        private async ValueTask<ContributionTypeDependencyException>
+            CreateAndLogCriticalDependencyExceptionAsync(Xeption exception)
+        {
+            var contributionTypeDependencyException = new ContributionTypeDependencyException(
+                message: "ContributionType dependency error occurred, contact support.",
+                innerException: exception);
+
+            await this.loggingBroker.LogCriticalAsync(contributionTypeDependencyException);
+
+            return contributionTypeDependencyException;
         }
     }
 }
