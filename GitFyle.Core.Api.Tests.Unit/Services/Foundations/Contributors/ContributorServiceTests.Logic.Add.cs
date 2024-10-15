34a3a31b4595c6ed6c58fca26e7674dea667f168
slimahmad6 <slimahmad6@outlook.com>
2024-10-15 12:00:57 +0100

    ShouldAddContributorAsync -> FAIL

---
+++ GitFyle.Core.Api.Tests.Unit/Services/Foundations/Contributors/ContributorServiceTests.Logic.Add.cs
@@ -0,0 +1,55 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Force.DeepCloner;
+using GitFyle.Core.Api.Models.Foundations.Contributors;
+using Moq;
+
+namespace GitFyle.Core.Api.Tests.Unit.Services.Foundations.Contributors
+{
+    public partial class ContributorServiceTests
+    {
+        [Fact]
+        public async Task ShouldAddContributorAsync()
+        {
+            // given
+            DateTimeOffset randomDateTime = GetRandomDateTimeOffset();
+            DateTimeOffset now = randomDateTime;
+            Contributor randomContributor = CreateRandomContributor(dateTimeOffset: now);
+            Contributor inputContributor = randomContributor;
+            Contributor insertedContributor = inputContributor.DeepClone();
+            Contributor expectedContributor = insertedContributor.DeepClone();
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.InsertContributorAsync(inputContributor))
+                    .ReturnsAsync(insertedContributor);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTimeOffsetAsync())
+                    .ReturnsAsync(now);
+
+            // when
+            Contributor actualContributor =
+                await this.contributorService.AddContributorAsync(inputContributor);
+
+            // then
+            actualContributor.Should().BeEquivalentTo(expectedContributor);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffsetAsync(),
+                    Times.Never);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertContributorAsync(inputContributor),
+                    Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
