cd59805c62e909abb950ab3718954bf34a2a0b65
slimahmad6 <slimahmad6@outlook.com>
2024-10-15 13:54:02 +0100

    ShouldThrowCriticalDependencyExceptionOnAddIfSqlErrorOccurredAndLogItAsync -> PASS

---
+++ GitFyle.Core.Api/Models/Foundations/Contributors/Exceptions/ContributorDependencyValidationException.cs
@@ -0,0 +1,16 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System.Collections;
+using Xeptions;
+
+namespace GitFyle.Core.Api.Models.Foundations.Contributors.Exceptions
+{
+    public class ContributorDependencyValidationException : Xeption
+    {
+        public ContributorDependencyValidationException(string message, Xeption innerException, IDictionary data)
+            : base(message, innerException, data)
+        { }
+    }
+}
