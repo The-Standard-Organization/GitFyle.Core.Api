e4af4bc61cb28c0991454a1d6ece60c45e17f04f
slimahmad6 <slimahmad6@outlook.com>
2024-10-15 12:15:57 +0100

    ShouldThrowValidationExceptionOnAddIfContributorIsNullAndLogItAsync -> FAIL

---
+++ GitFyle.Core.Api.Tests.Unit/Services/Foundations/Contributors/ContributorServiceTests.Validations.Add.cs
@@ -0,0 +1,56 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using GitFyle.Core.Api.Models.Foundations.Contributors;
+using GitFyle.Core.Api.Models.Foundations.Contributors.Exceptions;
+using Moq;
+
+namespace GitFyle.Core.Api.Tests.Unit.Services.Foundations.Contributors
+{
+    public partial class ContributorServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnAddIfContributorIsNullAndLogItAsync()
+        {
+            // given
+            Contributor nullContributor = null;
+
+            var nullContributorException =
+                new NullContributorException(
+                    message: "Contributor is null");
+
+            var expectedContributorValidationException =
+                new ContributorValidationException(
+                    message: "Contributor validation error occurred, fix errors and try again.",
+                    innerException: nullContributorException);
+
+            // when
+            ValueTask<Contributor> addContributorTask =
+                this.contributorService.AddContributorAsync(nullContributor);
+
+            ContributorValidationException actualContributorValidationException =
+                await Assert.ThrowsAsync<ContributorValidationException>(
+                    testCode: addContributorTask.AsTask);
+
+            // then
+            actualContributorValidationException.Should().BeEquivalentTo(
+                expectedContributorValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(
+                    SameExceptionAs(expectedContributorValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertContributorAsync(It.IsAny<Contributor>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
