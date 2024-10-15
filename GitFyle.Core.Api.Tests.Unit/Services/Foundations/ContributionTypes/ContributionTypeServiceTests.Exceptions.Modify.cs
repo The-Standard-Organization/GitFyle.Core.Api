45cf2c223a8e61ac5601681b630878db92ffc32e
slimahmad6 <slimahmad6@outlook.com>
2024-10-15 11:02:56 +0100

    ShouldThrowCriticalDependencyExceptionOnModifyIfSqlErrorOccursAndLogItAsync - > PASS

---
+++ GitFyle.Core.Api.Tests.Unit/Services/Foundations/ContributionTypes/ContributionTypeServiceTests.Exceptions.Modify.cs
@@ -0,0 +1,73 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using GitFyle.Core.Api.Models.Foundations.ContributionTypes;
+using GitFyle.Core.Api.Models.Foundations.ContributionTypes.Exceptions;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+
+namespace GitFyle.Core.Api.Tests.Unit.Services.Foundations.ContributionTypes
+{
+    public partial class ContributionTypeServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowCriticalDependencyExceptionOnModifyIfSqlErrorOccursAndLogItAsync()
+        {
+            // given
+            ContributionType someContributionType = CreateRandomContributionType();
+            SqlException sqlException = CreateSqlException();
+
+            var failedContributionTypeStorageException =
+                new FailedStorageContributionTypeException(
+                    message: "Failed storage contributionType error occurred, contact support.",
+                        innerException: sqlException);
+
+            var expectedContributionTypeDependencyException =
+                new ContributionTypeDependencyException(
+                    message: "ContributionType dependency error occurred, contact support.",
+                        innerException: failedContributionTypeStorageException);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTimeOffsetAsync())
+                    .ThrowsAsync(sqlException);
+
+            // when
+            ValueTask<ContributionType> modifyContributionTypeTask =
+                this.contributionTypeService.ModifyContributionTypeAsync(someContributionType);
+
+            ContributionTypeDependencyException actualContributionTypeDependencyException =
+                await Assert.ThrowsAsync<ContributionTypeDependencyException>(
+                    testCode: modifyContributionTypeTask.AsTask);
+
+            // then
+            actualContributionTypeDependencyException.Should().BeEquivalentTo(
+                expectedContributionTypeDependencyException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffsetAsync(),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectContributionTypeByIdAsync(someContributionType.Id),
+                    Times.Never);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCriticalAsync(It.Is(SameExceptionAs(
+                    expectedContributionTypeDependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateContributionTypeAsync(someContributionType),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
