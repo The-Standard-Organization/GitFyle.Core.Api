d1d239024363985cbdfcc76bbab8fad52c7a8af6
slimahmad6 <slimahmad6@outlook.com>
2024-10-15 13:56:21 +0100

    Reapply "ShouldThrowCriticalDependencyExceptionOnAddIfSqlErrorOccurredAndLogItAsync -> PASS"
    This reverts commit ddad91f96deddc7df3bca40122c90d444196defb.

---
+++ GitFyle.Core.Api/Models/Foundations/Contributors/Exceptions/ContributorServiceException.cs
@@ -0,0 +1,15 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using Xeptions;
+
+namespace GitFyle.Core.Api.Models.Foundations.Contributors.Exceptions
+{
+    public class ContributorServiceException : Xeption
+    {
+        public ContributorServiceException(string message, Xeption innerException)
+            : base(message, innerException)
+        { }
+    }
+}
