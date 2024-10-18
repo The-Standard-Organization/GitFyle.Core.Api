a5331fa0f7ad2b1f2bf747733988fc28c6cec4b4
Pratik Kaje <kajepratik@gmail.com>
2024-10-18 20:40:06 +0400

    ShouldReturnInternalServerErrorOnDeleteIfServerErrorOccurredAsync => FAIL

--- GitFyle.Core.Api.Tests.Unit/Controllers/Configurations/ConfigurationsControllerTests.Exceptions.Delete.cs
+++ GitFyle.Core.Api.Tests.Unit/Controllers/Configurations/ConfigurationsControllerTests.Exceptions.Delete.cs
@@ -11,6 +11,7 @@ using GitFyle.Core.Api.Models.Foundations.Configurations;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using RESTFulSense.Clients.Extensions;
+using RESTFulSense.Models;
 using Xeptions;
 
 namespace GitFyle.Core.Api.Tests.Unit.Controllers.Configurations
@@ -26,6 +27,38 @@ namespace GitFyle.Core.Api.Tests.Unit.Controllers.Configurations
 
             BadRequestObjectResult expectedBadRequestObjectResult =
                 BadRequest(validationException.InnerException);
+
+            var expectedActionResult =
+                new ActionResult<Configuration>(expectedBadRequestObjectResult);
+
+            this.configurationServiceMock.Setup(service =>
+                service.RemoveConfigurationByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(validationException);
+
+            // when
+            ActionResult<Configuration> actualActionResult =
+                await this.configurationsController.DeleteConfigurationByIdAsync(someId);
+
+            // then
+            actualActionResult.ShouldBeEquivalentTo(expectedActionResult);
+
+            this.configurationServiceMock.Verify(service =>
+                service.RemoveConfigurationByIdAsync(It.IsAny<Guid>()),
+                    Times.Once);
+
+            this.configurationServiceMock.VerifyNoOtherCalls();
+        }
+
+        [Theory]
+        [MemberData(nameof(ServerExceptions))]
+        public async Task ShouldReturnInternalServerErrorOnDeleteIfServerErrorOccurredAsync(
+            Xeption validationException)
+        {
+            // given
+            Guid someId = Guid.NewGuid();
+
+            InternalServerErrorObjectResult expectedBadRequestObjectResult =
+                InternalServerError(validationException);
 
             var expectedActionResult =
                 new ActionResult<Configuration>(expectedBadRequestObjectResult);
