32edc9d71833cacccabf166f46da2d34df321777
slimahmad6 <slimahmad6@outlook.com>
2024-11-14 10:09:28 +0100

    ShouldThrowCriticalDependencyExceptionOnRetrieveAllWhenSQLExceptionOccursAndLogItAsync -> FAIL

---
+++ GitFyle.Core.Api.Tests.Unit/Services/Foundations/Contributors/ContributorServiceTests.Exceptions.RetrieveAll.cs
@@ -0,0 +1,68 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using GitFyle.Core.Api.Models.Foundations.Contributors;
+using GitFyle.Core.Api.Models.Foundations.Contributors.Exceptions;
+using Microsoft.Data.SqlClient;
+using Moq;
+
+namespace GitFyle.Core.Api.Tests.Unit.Services.Foundations.Contributors
+{
+    public partial class ContributorServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowCriticalDependencyExceptionOnRetrieveAllWhenSQLExceptionOccursAndLogItAsync()
+        {
+            // given
+            SqlException sqlException = CreateSqlException();
+
+            var failedStorageContributorException =
+                new FailedStorageContributorException(
+                    message: "Failed contributor storage error occurred, contact support.",
+                    innerException: sqlException);
+
+            var expectedContributorDependencyException =
+                new ContributorDependencyException(
+                    message: "Contributor dependency error occurred, contact support.",
+                    innerException: failedStorageContributorException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllContributorsAsync())
+                    .ThrowsAsync(sqlException);
+
+            // when
+            ValueTask<IQueryable<Contributor>> retrieveAllContributorsTask =
+                this.contributorService.RetrieveAllContributorsAsync();
+
+            ContributorDependencyException actualContributorDependencyException =
+                await Assert.ThrowsAsync<ContributorDependencyException>(
+                    testCode: retrieveAllContributorsTask.AsTask);
+
+            // then
+            actualContributorDependencyException.Should().BeEquivalentTo(
+                expectedContributorDependencyException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllContributorsAsync(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCriticalAsync(It.Is(SameExceptionAs(
+                    expectedContributorDependencyException))),
+                        Times.Once);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffsetAsync(),
+                    Times.Never);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
