4da90ee4cc9d790ea8e767e013471f6359029f0b
slimahmad6 <slimahmad6@outlook.com>
2024-11-20 10:45:31 +0100

    ShouldThrowValidationExceptionOnModifyIfContributorHasInvalidLengthPropertiesAndLogItAsync -> PASS

--- GitFyle.Core.Api.Tests.Unit/Services/Foundations/Contributors/ContributorServiceTests.Validations.Modify.cs
+++ GitFyle.Core.Api.Tests.Unit/Services/Foundations/Contributors/ContributorServiceTests.Validations.Modify.cs
@@ -157,5 +157,79 @@ namespace GitFyle.Core.Api.Tests.Unit.Services.Foundations.Contributors
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnModifyIfContributorHasInvalidLengthPropertiesAndLogItAsync()
+        {
+            // given
+            DateTimeOffset randomDateTimeOffset = GetRandomDateTimeOffset();
+            var invalidContributor = CreateRandomContributor(dateTimeOffset: randomDateTimeOffset);
+            invalidContributor.ExternalId = GetRandomStringWithLengthOf(256);
+            invalidContributor.Username = GetRandomStringWithLengthOf(256);
+            invalidContributor.Name = GetRandomStringWithLengthOf(256);
+            invalidContributor.Email = GetRandomStringWithLengthOf(256);
+
+            var invalidContributorException =
+                new InvalidContributorException(
+                    message: "Contributor is invalid, fix the errors and try again.");
+
+            invalidContributorException.AddData(
+                key: nameof(Contributor.ExternalId),
+                values: $"Text exceeds max length of {invalidContributor.ExternalId.Length - 1} characters");
+
+            invalidContributorException.AddData(
+                key: nameof(Contributor.Username),
+                values: $"Text exceeds max length of {invalidContributor.Username.Length - 1} characters");
+
+            invalidContributorException.AddData(
+                key: nameof(Contributor.Name),
+                values: $"Text exceeds max length of {invalidContributor.Name.Length - 1} characters");
+
+            invalidContributorException.AddData(
+                key: nameof(Contributor.Email),
+                values: $"Text exceeds max length of {invalidContributor.Email.Length - 1} characters");
+
+            invalidContributorException.AddData(
+                key: nameof(Contributor.UpdatedDate),
+                values: $"Date is the same as {nameof(Contributor.CreatedDate)}");
+
+            var expectedContributorValidationException =
+                new ContributorValidationException(
+                    message: "Contributor validation error occurred, fix errors and try again.",
+                    innerException: invalidContributorException);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTimeOffsetAsync())
+                    .ReturnsAsync(randomDateTimeOffset);
+
+            // when
+            ValueTask<Contributor> modifyContributorTask =
+                this.contributorService.ModifyContributorAsync(invalidContributor);
+
+            ContributorValidationException actualContributorValidationException =
+                await Assert.ThrowsAsync<ContributorValidationException>(
+                    testCode: modifyContributorTask.AsTask);
+
+            // then
+            actualContributorValidationException.Should()
+                .BeEquivalentTo(expectedContributorValidationException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffsetAsync(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedContributorValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertContributorAsync(It.IsAny<Contributor>()),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
