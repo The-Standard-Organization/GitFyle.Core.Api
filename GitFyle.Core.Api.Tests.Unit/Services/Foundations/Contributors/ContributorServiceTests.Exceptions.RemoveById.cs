10e9eabeec526acd1165b3fe5906f83791a46a54
slimahmad6 <slimahmad6@outlook.com>
2024-11-20 11:02:24 +0100

    ShouldThrowDependencyValidationExceptionOnRemoveByIdIfDbConcurrencyOccursAndLogItAsync -> PASS

--- GitFyle.Core.Api.Tests.Unit/Services/Foundations/Contributors/ContributorServiceTests.Exceptions.RemoveById.cs
+++ GitFyle.Core.Api.Tests.Unit/Services/Foundations/Contributors/ContributorServiceTests.Exceptions.RemoveById.cs
@@ -65,5 +65,60 @@ namespace GitFyle.Core.Api.Tests.Unit.Services.Foundations.Contributors
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        private async Task ShouldThrowDependencyValidationExceptionOnRemoveByIdIfDbConcurrencyOccursAndLogItAsync()
+        {
+            // given
+            Guid someContributorId = Guid.NewGuid();
+
+            var dbUpdateConcurrencyException =
+                new DbUpdateConcurrencyException();
+
+            var lockedContributorException =
+                new LockedContributorException(
+                    message: "Locked contributor record error occurred, please try again.",
+                    innerException: dbUpdateConcurrencyException,
+                    data: dbUpdateConcurrencyException.Data);
+
+            var expectedContributorDependencyValidationException =
+                new ContributorDependencyValidationException(
+                    message: "Contributor dependency validation error occurred, fix errors and try again.",
+                    innerException: lockedContributorException,
+                    data: lockedContributorException.Data);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectContributorByIdAsync(someContributorId))
+                    .ThrowsAsync(dbUpdateConcurrencyException);
+
+            // when
+            ValueTask<Contributor> removeContributorByIdTask =
+                this.contributorService.RemoveContributorByIdAsync(someContributorId);
+
+            ContributorDependencyValidationException actualContributorDependencyValidationException =
+                await Assert.ThrowsAsync<ContributorDependencyValidationException>(
+                    removeContributorByIdTask.AsTask);
+
+            // then
+            actualContributorDependencyValidationException.Should().BeEquivalentTo(
+                expectedContributorDependencyValidationException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectContributorByIdAsync(It.IsAny<Guid>()),
+                    Times.Once());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(SameExceptionAs(
+                    expectedContributorDependencyValidationException))),
+                        Times.Once);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffsetAsync(),
+                    Times.Never);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
