bd2235554a07a2d56d7fd09d44f22686cd5fc9af
slimahmad6 <slimahmad6@outlook.com>
2024-11-26 10:21:55 +0100

    ShouldReturnOkWithRepositoriesOnGetAsync -> PASS

---
+++ GitFyle.Core.Api/Controllers/RepositoriesController.cs
@@ -0,0 +1,32 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System.Linq;
+using System.Threading.Tasks;
+using GitFyle.Core.Api.Models.Foundations.Repositories;
+using GitFyle.Core.Api.Services.Foundations.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+
+namespace GitFyle.Core.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RepositoriesController : RESTFulController
+    {
+        private readonly IRepositoryService repositoryService;
+
+        public RepositoriesController(IRepositoryService repositoryService) =>
+            this.repositoryService = repositoryService;
+
+        [HttpGet]
+        public async ValueTask<ActionResult<IQueryable<Repository>>> GetRepositoriesAsync()
+        {
+                IQueryable<Repository> repositories =
+                    await this.repositoryService.RetrieveAllRepositoriesAsync();
+
+                return Ok(repositories);
+        }
+    }
+}
