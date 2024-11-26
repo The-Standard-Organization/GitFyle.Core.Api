86cab7f5f1b301f9cfbc6d27401be68390b0f900
slimahmad6 <slimahmad6@outlook.com>
2024-11-26 10:58:42 +0100

    ShouldReturnBadRequestOnGetByIdIfValidationErrorOccursAsync -> PASS

--- GitFyle.Core.Api/Controllers/RepositoriesController.cs
+++ GitFyle.Core.Api/Controllers/RepositoriesController.cs
@@ -45,10 +45,26 @@ namespace GitFyle.Core.Api.Controllers
         [HttpGet("{repositoryId}")]
         public async ValueTask<ActionResult<Repository>> GetRepositoryByIdAsync(Guid repositoryId)
         {
+            try
+            {
                 Repository repository =
                     await this.repositoryService.RetrieveRepositoryByIdAsync(repositoryId);
 
                 return Ok(repository);
+            }
+            catch (RepositoryValidationException repositoryValidationException)
+                when (repositoryValidationException.InnerException is NotFoundRepositoryException)
+            {
+                return NotFound(repositoryValidationException.InnerException);
+            }
+            catch (RepositoryValidationException repositoryValidationException)
+            {
+                return BadRequest(repositoryValidationException.InnerException);
+            }
+            catch (RepositoryDependencyValidationException repositoryDependencyValidationException)
+            {
+                return BadRequest(repositoryDependencyValidationException.InnerException);
+            }
         }
     }
 }
