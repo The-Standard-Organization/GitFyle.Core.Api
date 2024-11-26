99c0c644640d8e5bd41b5a82203198e542a44aa1
slimahmad6 <slimahmad6@outlook.com>
2024-11-26 10:23:15 +0100

    ShouldReturnInternalServerErrorOnGetIfServerErrorOccurredAsync -> PASS

--- GitFyle.Core.Api/Controllers/RepositoriesController.cs
+++ GitFyle.Core.Api/Controllers/RepositoriesController.cs
@@ -2,9 +2,11 @@
 // Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
 // ----------------------------------------------------------------------------------
 
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 using GitFyle.Core.Api.Models.Foundations.Repositories;
+using GitFyle.Core.Api.Models.Foundations.Repositories.Exceptions;
 using GitFyle.Core.Api.Services.Foundations.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using RESTFulSense.Controllers;
@@ -23,10 +25,21 @@ namespace GitFyle.Core.Api.Controllers
         [HttpGet]
         public async ValueTask<ActionResult<IQueryable<Repository>>> GetRepositoriesAsync()
         {
+            try
+            {
                 IQueryable<Repository> repositories =
                     await this.repositoryService.RetrieveAllRepositoriesAsync();
 
                 return Ok(repositories);
+            }
+            catch (RepositoryDependencyException repositoryDependencyException)
+            {
+                return InternalServerError(repositoryDependencyException);
+            }
+            catch (RepositoryServiceException repositoryServiceException)
+            {
+                return InternalServerError(repositoryServiceException);
+            }
         }
     }
 }
