aa5aacdb4e6b09827d8361e4e952076a33319384
slimahmad6 <slimahmad6@outlook.com>
2024-11-26 11:28:52 +0100

    ShouldReturnInternalServerErrorOnDeleteIfServerErrorOccurredAsync -> FAIL

--- GitFyle.Core.Api.Tests.Unit/Controllers/Repositories/RepositoriesControllerTests.Exceptions.Delete.cs
+++ GitFyle.Core.Api.Tests.Unit/Controllers/Repositories/RepositoriesControllerTests.Exceptions.Delete.cs
@@ -16,6 +16,38 @@ namespace GitFyle.Core.Api.Tests.Unit.Controllers.Repositories
 {
     public partial class RepositoriesControllerTests
     {
+        [Theory]
+        [MemberData(nameof(ServerExceptions))]
+        public async Task ShouldReturnInternalServerErrorOnDeleteIfServerErrorOccurredAsync(
+            Xeption validationException)
+        {
+            // given
+            Guid someId = Guid.NewGuid();
+
+            InternalServerErrorObjectResult expectedBadRequestObjectResult =
+                InternalServerError(validationException);
+
+            var expectedActionResult =
+                new ActionResult<Repository>(expectedBadRequestObjectResult);
+
+            this.repositoryServiceMock.Setup(service =>
+                service.RemoveRepositoryByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(validationException);
+
+            // when
+            ActionResult<Repository> actualActionResult =
+                await this.repositoriesController.DeleteRepositoryByIdAsync(someId);
+
+            // then
+            actualActionResult.ShouldBeEquivalentTo(expectedActionResult);
+
+            this.repositoryServiceMock.Verify(service =>
+                service.RemoveRepositoryByIdAsync(It.IsAny<Guid>()),
+                    Times.Once);
+
+            this.repositoryServiceMock.VerifyNoOtherCalls();
+        }
+
         [Fact]
         public async Task ShouldReturnNotFoundOnDeleteIfItemDoesNotExistAsync()
         {
