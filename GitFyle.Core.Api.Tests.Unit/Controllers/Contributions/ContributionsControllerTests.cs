e8b6f9111cbfd6269676801918eef5487dd4bf69
Abdulsamad Osunlana <osunlanaabdulsamad@gmail.com>
2025-02-02 18:20:59 +0100

    ShouldReturnOkOnGetAllContributionsAsync -> FAIL

---
+++ GitFyle.Core.Api.Tests.Unit/Controllers/Contributions/ContributionsControllerTests.cs
@@ -0,0 +1,59 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Linq;
+using GitFyle.Core.Api.Controllers;
+using GitFyle.Core.Api.Models.Foundations.Contributions;
+using GitFyle.Core.Api.Services.Foundations.Contributions;
+using Moq;
+using RESTFulSense.Controllers;
+using Tynamix.ObjectFiller;
+
+namespace GitFyle.Core.Api.Tests.Unit.Controllers.Contributions
+{
+    public partial class ContributionsControllerTests : RESTFulController
+    {
+        private readonly Mock<IContributionService> contributionServiceMock;
+        private readonly ContributionsController contributionsController;
+
+        public ContributionsControllerTests()
+        {
+            this.contributionServiceMock = new Mock<IContributionService>();
+
+            this.contributionsController = new ContributionsController(
+                contributionService: this.contributionServiceMock.Object);
+        }
+
+        private static int GetRandomNumber() =>
+            new IntRange(min: 2, max: 9).GetValue();
+
+        private static DateTimeOffset GetRandomDateTimeOffset() =>
+            new DateTimeRange(earliestDate: DateTime.UnixEpoch).GetValue();
+
+        private static IQueryable<Contribution> CreateRandomnContributions() =>
+            CreateRandomContributions().Create(count: GetRandomNumber()).AsQueryable();
+
+        private static Filler<Contribution> CreateRandomContributions()
+        {
+            var filler = new Filler<Contribution>();
+
+            filler.Setup()
+                .OnProperty(contribution =>
+                    contribution.ContributionType).IgnoreIt()
+
+                .OnProperty(contribution =>
+                    contribution.Contributor).IgnoreIt()
+
+                .OnProperty(contribution =>
+                    contribution.Repository).IgnoreIt()
+
+                .OnType<DateTimeOffset>().Use(GetRandomDateTimeOffset);
+
+
+
+            return filler;
+        }
+    }
+}
