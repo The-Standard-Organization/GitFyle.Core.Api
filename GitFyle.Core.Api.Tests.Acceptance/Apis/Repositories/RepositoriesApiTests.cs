de8e80bdceed3989dc857f4374ef198ec4eae2e3
Pratik Kaje <kajepratik@gmail.com>
2025-03-03 22:44:01 +0400

    CODE RUB: Code Cleanup

--- GitFyle.Core.Api.Tests.Acceptance/Apis/Repositories/RepositoriesApiTests.cs
+++ GitFyle.Core.Api.Tests.Acceptance/Apis/Repositories/RepositoriesApiTests.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 using System.Threading.Tasks;
 using GitFyle.Core.Api.Tests.Acceptance.Brokers;
 using GitFyle.Core.Api.Tests.Acceptance.Models.Repositories;
--- GitFyle.Core.Api.Tests.Acceptance/Brokers/GitFyleCoreApiBroker.Repositories.cs
+++ GitFyle.Core.Api.Tests.Acceptance/Brokers/GitFyleCoreApiBroker.Repositories.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using GitFyle.Core.Api.Tests.Acceptance.Models.Repositories;
 
