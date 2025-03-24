c861cb9b45e1183ba81a77346f4f568d0733112a
slimahmad6 <slimahmad6@outlook.com>
2025-03-24 15:05:09 +0100

    ShouldReturnInternalServerErrorOnPostIfServerErrorOccurredAsync -> FAIL

--- GitFyle.Core.Api.Tests.Unit/Controllers/Contributions/ContributionsControllerTests.Exceptions.Post.cs
+++ GitFyle.Core.Api.Tests.Unit/Controllers/Contributions/ContributionsControllerTests.Exceptions.Post.cs
@@ -2,10 +2,8 @@
 // Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
 // ----------------------------------------------------------------------------------
 
-using System;
 using System.Threading.Tasks;
 using GitFyle.Core.Api.Models.Foundations.Contributions;
-using GitFyle.Core.Api.Models.Foundations.Contributions.Exceptions;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using RESTFulSense.Clients.Extensions;
@@ -32,6 +30,38 @@ namespace GitFyle.Core.Api.Tests.Unit.Controllers.Contributions
             this.contributionServiceMock.Setup(service =>
                 service.AddContributionAsync(It.IsAny<Contribution>()))
                     .ThrowsAsync(validationException);
+
+            // when
+            ActionResult<Contribution> actualActionResult =
+                await this.contributionsController.PostContributionAsync(someContribution);
+
+            // then
+            actualActionResult.ShouldBeEquivalentTo(expectedActionResult);
+
+            this.contributionServiceMock.Verify(service =>
+                service.AddContributionAsync(It.IsAny<Contribution>()),
+                    Times.Once);
+
+            this.contributionServiceMock.VerifyNoOtherCalls();
+        }
+
+        [Theory]
+        [MemberData(nameof(ServerExceptions))]
+        public async Task ShouldReturnInternalServerErrorOnPostIfServerErrorOccurredAsync(
+            Xeption serverException)
+        {
+            // given
+            Contribution someContribution = CreateRandomContribution();
+
+            InternalServerErrorObjectResult expectedInternalServerErrorObjectResult =
+                InternalServerError(serverException);
+
+            var expectedActionResult =
+                new ActionResult<Contribution>(expectedInternalServerErrorObjectResult);
+
+            this.contributionServiceMock.Setup(service =>
+                service.AddContributionAsync(It.IsAny<Contribution>()))
+                    .ThrowsAsync(serverException);
 
             // when
             ActionResult<Contribution> actualActionResult =
