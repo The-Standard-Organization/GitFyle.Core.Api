3c62b6dad052ccd657e011907e151d6a7527b7c5
slimahmad6 <slimahmad6@outlook.com>
2025-03-24 15:48:15 +0100

    ShouldReturnBadRequestOnPostIfValidationErrorOccursAsync -> PASS

--- GitFyle.Core.Api/Controllers/ContributionTypesController.cs
+++ GitFyle.Core.Api/Controllers/ContributionTypesController.cs
@@ -2,8 +2,11 @@
 // Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
 // ----------------------------------------------------------------------------------
 
+using System;
+using System.Linq;
 using System.Threading.Tasks;
 using GitFyle.Core.Api.Models.Foundations.ContributionTypes;
+using GitFyle.Core.Api.Models.Foundations.ContributionTypes.Exceptions;
 using GitFyle.Core.Api.Services.Foundations.ContributionTypes;
 using Microsoft.AspNetCore.Mvc;
 using RESTFulSense.Controllers;
@@ -22,10 +25,21 @@ namespace GitFyle.Core.Api.Controllers
         [HttpPost]
         public async ValueTask<ActionResult<ContributionType>> PostContributionTypeAsync(ContributionType contributionType)
         {
+            try
+            {
                 ContributionType addedContributionType =
                     await this.contributionTypeService.AddContributionTypeAsync(contributionType);
 
                 return Created(addedContributionType);
+            }
+            catch (ContributionTypeValidationException contributionTypeValidationException)
+            {
+                return BadRequest(contributionTypeValidationException.InnerException);
+            }
+            catch (ContributionTypeDependencyValidationException contributionTypeDependencyValidationException)
+            {
+                return BadRequest(contributionTypeDependencyValidationException.InnerException);
+            }
         }
 
     }
