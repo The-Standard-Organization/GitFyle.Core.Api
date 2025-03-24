318d30a92a6291d09221c7364c7104ed17dc4b5a
slimahmad6 <slimahmad6@outlook.com>
2025-03-24 15:49:13 +0100

    ShouldReturnInternalServerErrorOnPostIfServerErrorOccurredAsync -> FAIL

--- GitFyle.Core.Api.Tests.Unit/Controllers/ContributionTypes/ContributionTypesControllerTests.Exceptions.Post.cs
+++ GitFyle.Core.Api.Tests.Unit/Controllers/ContributionTypes/ContributionTypesControllerTests.Exceptions.Post.cs
@@ -7,6 +7,7 @@ using GitFyle.Core.Api.Models.Foundations.ContributionTypes;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using RESTFulSense.Clients.Extensions;
+using RESTFulSense.Models;
 using Xeptions;
 
 namespace GitFyle.Core.Api.Tests.Unit.Controllers.ContributionTypes
@@ -29,6 +30,38 @@ namespace GitFyle.Core.Api.Tests.Unit.Controllers.ContributionTypes
             this.contributionTypeServiceMock.Setup(service =>
                 service.AddContributionTypeAsync(It.IsAny<ContributionType>()))
                     .ThrowsAsync(validationException);
+
+            // when
+            ActionResult<ContributionType> actualActionResult =
+                await this.repositoriesController.PostContributionTypeAsync(someContributionType);
+
+            // then
+            actualActionResult.ShouldBeEquivalentTo(expectedActionResult);
+
+            this.contributionTypeServiceMock.Verify(service =>
+                service.AddContributionTypeAsync(It.IsAny<ContributionType>()),
+                    Times.Once);
+
+            this.contributionTypeServiceMock.VerifyNoOtherCalls();
+        }
+
+        [Theory]
+        [MemberData(nameof(ServerExceptions))]
+        public async Task ShouldReturnInternalServerErrorOnPostIfServerErrorOccurredAsync(
+            Xeption serverException)
+        {
+            // given
+            ContributionType someContributionType = CreateRandomContributionType();
+
+            InternalServerErrorObjectResult expectedInternalServerErrorObjectResult =
+                InternalServerError(serverException);
+
+            var expectedActionResult =
+                new ActionResult<ContributionType>(expectedInternalServerErrorObjectResult);
+
+            this.contributionTypeServiceMock.Setup(service =>
+                service.AddContributionTypeAsync(It.IsAny<ContributionType>()))
+                    .ThrowsAsync(serverException);
 
             // when
             ActionResult<ContributionType> actualActionResult =
--- GitFyle.Core.Api/Controllers/ContributionTypesController.cs
+++ GitFyle.Core.Api/Controllers/ContributionTypesController.cs
@@ -2,8 +2,6 @@
 // Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
 // ----------------------------------------------------------------------------------
 
-using System;
-using System.Linq;
 using System.Threading.Tasks;
 using GitFyle.Core.Api.Models.Foundations.ContributionTypes;
 using GitFyle.Core.Api.Models.Foundations.ContributionTypes.Exceptions;
