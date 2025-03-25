50fb3ada802bd220e28555bcc684e21830119467
slimahmad6 <slimahmad6@outlook.com>
2025-03-25 10:49:22 +0100

    ShouldReturnNotFoundOnDeleteIfItemDoesNotExistAsync -> PASS

--- GitFyle.Core.Api/Controllers/ContributionTypesController.cs
+++ GitFyle.Core.Api/Controllers/ContributionTypesController.cs
@@ -157,6 +157,11 @@ namespace GitFyle.Core.Api.Controllers
                 return Ok(deleteContributionType);
             }
             catch (ContributionTypeValidationException contributionTypeValidationException)
+                when (contributionTypeValidationException.InnerException is NotFoundContributionTypeException)
+            {
+                return NotFound(contributionTypeValidationException.InnerException);
+            }
+            catch (ContributionTypeValidationException contributionTypeValidationException)
             {
                 return BadRequest(contributionTypeValidationException.InnerException);
             }
